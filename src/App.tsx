55dd163bced1a67dddd01241327eaae1c6456925
jatnerubia <jlerit@nerubia.com>
2023-10-02 14:22:50 +0800

    added router and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import "./App.css"
+import { Layout } from "./components/layouts/Layout"
+import { Home } from "./pages/home"
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,3 @@
+export const Home = () => {
+  return <div className='bg-blue-500'>Home</div>
+}
