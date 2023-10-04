9e5f1c6150060aad09efbd84efb2b9fc1decef46
jatnerubia <jlerit@nerubia.com>
2023-10-04 09:44:14 +0800

    updated reset password form

--- src/features/auth/reset-password/ResetPasswordForm.tsx
+++ src/features/auth/reset-password/ResetPasswordForm.tsx
@@ -5,9 +5,13 @@ export const ResetPasswordForm = () => {
   return (
     <div className='w-full md:w-96 flex flex-col gap-4 p-4 shadow-md'>
       <h1 className='text-lg font-bold'>Reset Password</h1>
-      <Input type='password' placeholder='New password' />
-      <Input type='password' placeholder='Confirm new password' />
-      <Button name='Reset' />
+      <Input type='password' placeholder='New password' onChange={() => {}} />
+      <Input
+        type='password'
+        placeholder='Confirm new password'
+        onChange={() => {}}
+      />
+      <Button name='Reset' onClick={() => {}} />
     </div>
   )
 }
