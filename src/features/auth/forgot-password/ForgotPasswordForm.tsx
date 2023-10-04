05a67fb3978ce7ba7a1999917b5b5ffb404dc881
jatnerubia <jlerit@nerubia.com>
2023-10-04 09:43:36 +0800

    updated forgot password page

--- src/features/auth/forgot-password/ForgotPasswordForm.tsx
+++ src/features/auth/forgot-password/ForgotPasswordForm.tsx
@@ -5,8 +5,8 @@ export const ForgotPasswordForm = () => {
   return (
     <div className='w-full md:w-96 flex flex-col gap-4 p-4 shadow-md'>
       <h1 className='text-lg font-bold'>Forgot Password</h1>
-      <Input type='email' placeholder='Email' />
-      <Button name='Send email' />
+      <Input type='email' placeholder='Email' onChange={() => {}} />
+      <Button name='Send email' onClick={() => {}} />
     </div>
   )
 }
