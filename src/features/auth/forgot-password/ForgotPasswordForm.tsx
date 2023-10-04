fbcb106fa6b2d71c5b65ef815d584fd2613f9455
jatnerubia <jlerit@nerubia.com>
2023-10-04 11:31:59 +0800

    updated forgot and reset password form

--- src/features/auth/forgot-password/ForgotPasswordForm.tsx
+++ src/features/auth/forgot-password/ForgotPasswordForm.tsx
@@ -5,7 +5,12 @@ export const ForgotPasswordForm = () => {
   return (
     <div className='w-full md:w-96 flex flex-col gap-4 p-4 shadow-md'>
       <h1 className='text-lg font-bold'>Forgot Password</h1>
-      <Input type='email' placeholder='Email' onChange={() => {}} />
+      <Input
+        name='email'
+        type='email'
+        placeholder='Email'
+        onChange={() => {}}
+      />
       <Button name='Send email' onClick={() => {}} />
     </div>
   )
--- src/features/auth/reset-password/ResetPasswordForm.tsx
+++ src/features/auth/reset-password/ResetPasswordForm.tsx
@@ -5,8 +5,14 @@ export const ResetPasswordForm = () => {
   return (
     <div className='w-full md:w-96 flex flex-col gap-4 p-4 shadow-md'>
       <h1 className='text-lg font-bold'>Reset Password</h1>
-      <Input type='password' placeholder='New password' onChange={() => {}} />
       <Input
+        name='new_password'
+        type='password'
+        placeholder='New password'
+        onChange={() => {}}
+      />
+      <Input
+        name='confirm_new_password'
         type='password'
         placeholder='Confirm new password'
         onChange={() => {}}
