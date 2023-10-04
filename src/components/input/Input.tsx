759c82d143b5e61529743411c58b66ee7bd104da
jatnerubia <jlerit@nerubia.com>
2023-10-04 11:31:23 +0800

    updated input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,16 +1,28 @@
 interface InputProps {
+  name: string
   type: string
   placeholder: string
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  error?: string
 }
 
-export const Input = ({ type, placeholder, onChange }: InputProps) => {
+export const Input = ({
+  name,
+  type,
+  placeholder,
+  onChange,
+  error,
+}: InputProps) => {
   return (
-    <input
-      className='border px-4 py-2 rounded-md'
-      type={type}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <div className='flex flex-col'>
+      <input
+        className='border px-4 py-2 rounded-md'
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      {error != null && <p className='text-red-500 text-sm'>{error}</p>}
+    </div>
   )
 }
