77579b0a4ac44586f3c0832abb69d273860ee7c7
jatnerubia <jlerit@nerubia.com>
2023-10-04 14:04:11 +0800

    added loading prop

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,17 @@
 interface ButtonProps {
   name: string
   onClick: () => void
+  loading?: boolean
 }
 
-export const Button = ({ name, onClick }: ButtonProps) => {
+export const Button = ({ name, onClick, loading }: ButtonProps) => {
   return (
     <button
       className='bg-blue-500 text-white px-4 py-2 rounded-md'
       onClick={onClick}
+      disabled={loading}
     >
-      {name}
+      {loading === true ? "Loading" : name}
     </button>
   )
 }
