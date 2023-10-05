d694fb3daade3d9796a3c65f48796d58b2d9105c
jatnerubia <jlerit@nerubia.com>
2023-10-05 13:45:53 +0800

    updated login test

--- __tests__/pages/login.spec.ts
+++ __tests__/pages/login.spec.ts
@@ -11,4 +11,20 @@ test.describe("Login", () => {
     ).toBeVisible()
     await expect(page.getByRole("button", { name: "Login" })).toBeVisible()
   })
+
+  test("should show validation errors", async ({ page }) => {
+    await page.goto("/auth/login")
+
+    await page.getByRole("button", { name: "Login" }).click()
+    await expect(page.getByText("Email is required")).toBeVisible()
+    await expect(page.getByText("Password is required")).toBeVisible()
+
+    await page.getByRole("textbox", { name: "Email" }).fill("email")
+    await page.getByRole("textbox", { name: "Password" }).fill("1234")
+    await page.getByRole("button", { name: "Login" }).click()
+    await expect(page.getByText("Invalid email format")).toBeVisible()
+    await expect(
+      page.getByText("Password must be at least 8 characters")
+    ).toBeVisible()
+  })
 })
