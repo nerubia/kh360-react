98a9db564420546cc726280d80fae12b5f268dda
jatnerubia <jlerit@nerubia.com>
2023-10-09 08:26:38 +0800

    moved google auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Provider } from "react-redux"
-import { GoogleOAuthProvider } from "@react-oauth/google"
 import "./App.css"
 import { store } from "./redux/store"
 import { setupAxiosInstance } from "./utils/axiosInstance"
@@ -9,11 +8,7 @@ function App() {
   setupAxiosInstance(store)
   return (
     <Provider store={store}>
-      <GoogleOAuthProvider
-        clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string}
-      >
-        <PersistLogin />
-      </GoogleOAuthProvider>
+      <PersistLogin />
     </Provider>
   )
 }
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,13 +1,18 @@
+import { GoogleOAuthProvider } from "@react-oauth/google"
 import { LoginForm } from "../../features/auth/login/LoginForm"
 import { LoginOptions } from "../../features/auth/login/LoginOptions"
 
-export const Login = () => {
+export default function Login() {
   return (
     <div className='flex justify-center pt-10 p-4'>
       <div className='w-full sm:w-96 flex flex-col p-4 shadow-md'>
         <LoginForm />
         <p className='py-5 text-sm text-center'>OR</p>
-        <LoginOptions />
+        <GoogleOAuthProvider
+          clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string}
+        >
+          <LoginOptions />
+        </GoogleOAuthProvider>
       </div>
     </div>
   )
