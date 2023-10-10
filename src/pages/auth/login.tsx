85822479a093bdb5dee2e61c48199e89cb7ad2f5
jatnerubia <jlerit@nerubia.com>
2023-10-10 09:02:48 +0800

    updated login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -10,7 +10,14 @@ export default function Login() {
     <div className='flex justify-center pt-10 p-4'>
       <div className='w-full sm:w-96 flex flex-col p-4 shadow-md'>
         <LoginForm />
-        <p className='py-5 text-sm text-center'>OR</p>
+        <div className='relative py-10'>
+          <div className='absolute inset-0 flex items-center'>
+            <span className='w-full border-t' />
+          </div>
+          <div className='relative flex justify-center text-xs uppercase'>
+            <span className='bg-white px-2'>Or continue with</span>
+          </div>
+        </div>
         <GoogleOAuthProvider
           clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string}
         >
