ef6a027ef59e0ca3be529177a55464f4e3b0f0f3
jatnerubia <jlerit@nerubia.com>
2023-10-10 16:00:35 +0800

    added star icon

--- src/components/icon/icons.ts
+++ src/components/icon/icons.ts
@@ -3,4 +3,5 @@ import { lazy } from "react"
 export const icons = {
   DashboardIcon: lazy(async () => await import("./icons/DashboardIcon")),
   GoogleIcon: lazy(async () => await import("./icons/GoogleIcon")),
+  StarIcon: lazy(async () => await import("./icons/StarIcon")),
 }
