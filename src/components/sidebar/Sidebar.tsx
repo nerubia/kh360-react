33afea053ea5454d65879a8b5093e294c55a7fff
jatnerubia <jlerit@nerubia.com>
2023-10-11 17:15:35 +0800

    updated sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import { Button, LinkButton } from "../button/Button"
 
 export const Sidebar = () => {
   const { activeSidebar } = useAppSelector((state) => state.app)
+  const { user } = useAppSelector((state) => state.auth)
 
   const appDispatch = useAppDispatch()
 
@@ -19,15 +20,18 @@ export const Sidebar = () => {
         activeSidebar ? "" : "w-64 -ml-64"
       } bg-primary-500 fixed z-10 w-full md:w-64 h-screen transition-all duration-300 $`}
     >
-      <div className='flex flex-col gap-5 p-5'>
-        <div className='block md:hidden'>
+      <div className='relative flex flex-col gap-5 p-5'>
+        <div className='block absolute top-5 md:hidden'>
           <Button variant='ghost' onClick={toggleSidebar}>
-            <Icon icon='Menu' />
+            <Icon icon='Close' />
           </Button>
         </div>
         <div className='flex justify-center'>
           <img className='h-20' src='/logo.png' />
         </div>
+        <h1 className='text-lg text-center font-bold'>
+          {user?.firstName} {user?.lastName}
+        </h1>
         <div className='flex flex-col gap-2'>
           <LinkButton fullWidth to='/dashboard'>
             Dashboard
