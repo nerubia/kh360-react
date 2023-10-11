c83abd3b1fe180f31143c332f0a1c358a7f22047
jatnerubia <jlerit@nerubia.com>
2023-10-11 17:15:13 +0800

    added close icon

--- src/components/icon/icons.ts
+++ src/components/icon/icons.ts
@@ -2,6 +2,7 @@ import { lazy } from "react"
 
 export const icons = {
   Back: lazy(async () => await import("./icons/BackIcon")),
+  Close: lazy(async () => await import("./icons/CloseIcon")),
   Dashboard: lazy(async () => await import("./icons/DashboardIcon")),
   Google: lazy(async () => await import("./icons/GoogleIcon")),
   Menu: lazy(async () => await import("./icons/MenuIcon")),
