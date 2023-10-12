6ff4588f44b1ea86825873e195b9a4fc322a43e0
jatnerubia <jlerit@nerubia.com>
2023-10-12 15:37:41 +0800

    added evaluation schema

---
+++ src/utils/validation/evaluations/createEvaluationSchema.ts
@@ -0,0 +1,10 @@
+import { object, string } from "yup"
+
+export const createEvaluationSchema = object().shape({
+  name: string().required("Email is required"),
+  eval_period_start_date: string().required("Start period is required"),
+  eval_period_end_date: string().required("End period is required"),
+  eval_schedule_start_date: string().required("Start schedule is required"),
+  eval_schedule_end_date: string().required("End schedule is required"),
+  remarks: string().required("Description is required"),
+})
