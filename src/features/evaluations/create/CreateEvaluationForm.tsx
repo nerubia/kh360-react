88d84719ffe73e00302309202a657c24221ed7a6
jatnerubia <jlerit@nerubia.com>
2023-10-12 15:19:16 +0800

    updated create evaluation form

--- src/features/evaluations/create/CreateEvaluationForm.tsx
+++ src/features/evaluations/create/CreateEvaluationForm.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react"
+import { useAppDispatch } from "../../../hooks/useAppDispatch"
+import { useAppSelector } from "../../../hooks/useAppSelector"
+import { createEvaluation } from "../../../redux/slices/evaluationsSlice"
+import { type Evaluation } from "../../../types/evaluationType"
 import { Button } from "../../../components/button/Button"
 import { Input } from "../../../components/input/Input"
 import { TextArea } from "../../../components/textarea/TextArea"
 
 export const CreateEvaluationForm = () => {
+  const appDispatch = useAppDispatch()
+  const { loading } = useAppSelector((state) => state.evaluations)
+
+  const [formData, setFormData] = useState<Evaluation>({
+    name: undefined,
+    eval_period_start_date: undefined,
+    eval_period_end_date: undefined,
+    eval_schedule_start_date: undefined,
+    eval_schedule_end_date: undefined,
+    remarks: undefined,
+  })
+
+  const handleSubmit = async () => {
+    // TODO: validate
+    await appDispatch(createEvaluation(formData))
+  }
+
+  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleTextAreaChange = async (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
   return (
     <div className='flex flex-col gap-4'>
       <h1 className='text-lg font-bold'>Evaluation</h1>
@@ -12,7 +46,7 @@ export const CreateEvaluationForm = () => {
             label='Evaluation name'
             name='name'
             placeholder='Evaluation name'
-            onChange={() => {}}
+            onChange={handleInputChange}
           />
           <div className='flex flex-col md:items-end md:flex-row gap-4'>
             <Input
@@ -20,13 +54,13 @@ export const CreateEvaluationForm = () => {
               name='period_start'
               type='date'
               placeholder='Evaluation period'
-              onChange={() => {}}
+              onChange={handleInputChange}
             />
             <Input
               name='period_end'
               type='date'
               placeholder='Evaluation period'
-              onChange={() => {}}
+              onChange={handleInputChange}
             />
           </div>
           <div className='flex flex-col md:items-end md:flex-row gap-4'>
@@ -35,13 +69,13 @@ export const CreateEvaluationForm = () => {
               name='schedule_start'
               type='date'
               placeholder='Evaluation schedule'
-              onChange={() => {}}
+              onChange={handleInputChange}
             />
             <Input
               name='schedule_end'
               type='date'
               placeholder='Evaluation schedule'
-              onChange={() => {}}
+              onChange={handleInputChange}
             />
           </div>
         </div>
@@ -50,7 +84,7 @@ export const CreateEvaluationForm = () => {
             label='Evaluation description/notes'
             name='description'
             placeholder='Some description'
-            onChange={() => {}}
+            onChange={handleTextAreaChange}
           />
         </div>
       </div>
@@ -67,7 +101,9 @@ export const CreateEvaluationForm = () => {
         onChange={() => {}}
       />
       <div className='text-right'>
-        <Button onClick={() => {}}>Create</Button>
+        <Button onClick={handleSubmit} loading={loading}>
+          Create
+        </Button>
       </div>
     </div>
   )
