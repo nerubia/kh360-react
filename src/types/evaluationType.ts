2f5cad470f8a0ba9d2007169d9dd7a6281269b75
jatnerubia <jlerit@nerubia.com>
2023-10-12 15:17:47 +0800

    added evaluation type

--- src/types/evaluationType.ts
+++ src/types/evaluationType.ts
@@ -1,3 +1,8 @@
 export interface Evaluation {
-  name: string
+  name?: string
+  eval_period_start_date?: Date
+  eval_period_end_date?: Date
+  eval_schedule_start_date?: Date
+  eval_schedule_end_date?: Date
+  remarks?: string
 }
