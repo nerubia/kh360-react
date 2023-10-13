672335c9c7b5a7bc83d2629b1176aca9b06f6e48
jatnerubia <jlerit@nerubia.com>
2023-10-13 13:34:39 +0800

    added select employees form

---
+++ src/features/evaluations/view/SelectEmployeesForm.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react"
+import { Button } from "../../../components/button/Button"
+import { Input } from "../../../components/input/Input"
+import { Select } from "../../../components/select/Select"
+import { getEmployees } from "../../../redux/slices/employeesSlice"
+import { useAppDispatch } from "../../../hooks/useAppDispatch"
+import { useAppSelector } from "../../../hooks/useAppSelector"
+import { useParams } from "react-router-dom"
+import { Checkbox } from "../../../components/checkbox/Checkbox"
+
+export const SelectEmployeesForm = () => {
+  const { id } = useParams()
+  const appDispatch = useAppDispatch()
+  const { employees } = useAppSelector((state) => state.employees)
+
+  const [selectedEmployees, setSelectedEmployees] = useState<number[]>([])
+
+  useEffect(() => {
+    void appDispatch(getEmployees())
+  }, [])
+
+  const handleClickCheckbox = (checked: boolean, employeeId: number) => {
+    if (checked) {
+      setSelectedEmployees((prev) => [...prev, employeeId])
+    } else {
+      setSelectedEmployees((prev) => prev.filter((id) => id !== employeeId))
+    }
+  }
+
+  const handlePreview = () => {
+    // eslint-disable-next-line no-console
+    console.log(`Evaluation ID: ${id}`)
+    // eslint-disable-next-line no-console
+    console.log(selectedEmployees)
+  }
+
+  return (
+    <div className='flex flex-col'>
+      <h1 className='text-lg font-bold'>Select Employees</h1>
+      <div className='flex justify-between'>
+        <div className='flex gap-2'>
+          <Input
+            label='Name/Email'
+            name='search'
+            placeholder='Search name or email'
+            onChange={() => {}}
+          />
+          <Select
+            label='Status'
+            name='status'
+            onChange={() => {}}
+            options={[
+              {
+                label: "Active",
+                value: "active",
+              },
+              {
+                label: "Other",
+                value: "other",
+              },
+            ]}
+          />
+        </div>
+        <Button onClick={() => {}}>Search</Button>
+      </div>
+      <table className='w-full table-fixed'>
+        <thead className='text-left'>
+          <tr>
+            <th>Add</th>
+            <th>Name</th>
+            <th>Date Started</th>
+            <th>Regularized Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>
+                <div className='w-fit'>
+                  <Checkbox
+                    checked={selectedEmployees.includes(employee.id)}
+                    onChange={(checked) =>
+                      handleClickCheckbox(checked, employee.id)
+                    }
+                  />
+                </div>
+              </td>
+              <td>
+                {employee.firstName} {employee.lastName}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Button onClick={handlePreview}>Preview</Button>
+    </div>
+  )
+}
--- src/pages/evaluations/[id]/employees/select_employees.tsx
+++ src/pages/evaluations/[id]/employees/select_employees.tsx
@@ -1,24 +1,20 @@
 import { useParams } from "react-router-dom"
-import { Button, LinkButton } from "../../../../components/button/Button"
+import { LinkButton } from "../../../../components/button/Button"
 import { Icon } from "../../../../components/icon/Icon"
 import { useAppDispatch } from "../../../../hooks/useAppDispatch"
 import { useAppSelector } from "../../../../hooks/useAppSelector"
 import { useEffect } from "react"
 import { getEvaluation } from "../../../../redux/slices/evaluationsSlice"
-import { getEmployees } from "../../../../redux/slices/employeesSlice"
-import { Input } from "../../../../components/input/Input"
-import { Select } from "../../../../components/select/Select"
+import { SelectEmployeesForm } from "../../../../features/evaluations/view/SelectEmployeesForm"
 
 export default function SelectEmployees() {
   const { id } = useParams()
   const appDispatch = useAppDispatch()
   const { loading, evaluation } = useAppSelector((state) => state.evaluations)
-  const { employees } = useAppSelector((state) => state.employees)
 
   useEffect(() => {
     if (id !== undefined) {
       void appDispatch(getEvaluation(id))
-      void appDispatch(getEmployees())
     }
   }, [id])
 
@@ -33,58 +29,7 @@ export default function SelectEmployees() {
       {!loading && evaluation !== null && (
         <div className='flex flex-col'>
           <h1 className='text-lg font-bold'>{evaluation.name}</h1>
-          <h1 className='text-lg font-bold'>Select Employees</h1>
-          <div className='flex justify-between'>
-            <div className='flex gap-2'>
-              <Input
-                label='Name/Email'
-                name='search'
-                placeholder='Search name or email'
-                onChange={() => {}}
-              />
-              <Select
-                label='Status'
-                name='status'
-                onChange={() => {}}
-                options={[
-                  {
-                    label: "Active",
-                    value: "active",
-                  },
-                  {
-                    label: "Other",
-                    value: "other",
-                  },
-                ]}
-              />
-            </div>
-            <Button onClick={() => {}}>Search</Button>
-          </div>
-          <table className='w-full table-fixed'>
-            <thead className='text-left'>
-              <tr>
-                <th>Add</th>
-                <th>Name</th>
-                <th>Date Started</th>
-                <th>Regularized Date</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employees.map((employee) => (
-                <tr key={employee.id}>
-                  <td>
-                    <Button variant='ghost' size='small' onClick={() => {}}>
-                      <Icon icon='Plus' />
-                    </Button>
-                  </td>
-                  <td>
-                    {employee.firstName} {employee.lastName}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <SelectEmployeesForm />
         </div>
       )}
     </div>
