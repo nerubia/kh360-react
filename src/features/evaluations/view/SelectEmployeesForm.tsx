4182c34c76a8b1f400adaf83504a0f8d2f1b2970
Niño Allen Ardiente <ardientenino@gmail.com>
2023-10-16 15:28:48 +0800

    Fixed eslint

--- src/features/evaluations/view/SelectEmployeesForm.tsx
+++ src/features/evaluations/view/SelectEmployeesForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getEmployees } from "../../../redux/slices/employeesSlice"
