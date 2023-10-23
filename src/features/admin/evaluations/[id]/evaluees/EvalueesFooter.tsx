ed120049ad72d2d48a88fddc51aa4b6479b9ec95
Emmalyn Acha <eacha@nerubia.com>
2023-10-23 18:24:13 +0800

    feat: add back action for evaluees screen

--- src/features/admin/evaluations/[id]/evaluees/EvalueesFooter.tsx
+++ src/features/admin/evaluations/[id]/evaluees/EvalueesFooter.tsx
@@ -1,25 +1,52 @@
 import { useParams } from "react-router-dom"
+import { useState } from "react"
 import { Button, LinkButton } from "../../../../../components/button/Button"
 import { Icon } from "../../../../../components/icon/Icon"
+import Dialog from "../../../../../components/dialog/Dialog"
 
 export const EvalueesFooter = () => {
   const { id } = useParams()
 
+  const [showDialog, setShowDialog] = useState<boolean>(false)
+
+  const toggleDialog = () => {
+    setShowDialog((prev) => !prev)
+  }
+
   return (
-    <div className='flex justify-between'>
-      <Button variant='primaryOutline'>Cancel & Exit</Button>
-      <div className='flex items-center gap-2'>
-        <LinkButton
-          testId='BackButton'
-          variant='primaryOutline'
-          to={`/admin/evaluations/${id}/select`}
-        >
-          <Icon icon='ChevronLeft' />
-        </LinkButton>
-        <Button variant='primaryOutline' disabled>
-          Generate Evaluations
+    <>
+      <div className='flex justify-between'>
+        <Button variant='primaryOutline' onClick={toggleDialog}>
+          Cancel & Exit
         </Button>
+        <div className='flex items-center gap-2'>
+          <LinkButton
+            testId='BackButton'
+            variant='primaryOutline'
+            to={`/admin/evaluations/${id}/select`}
+          >
+            <Icon icon='ChevronLeft' />
+          </LinkButton>
+          <Button variant='primaryOutline' disabled>
+            Generate Evaluations
+          </Button>
+        </div>
       </div>
-    </div>
+      <Dialog open={showDialog}>
+        <Dialog.Title>Cancel & Exit</Dialog.Title>
+        <Dialog.Description>
+          Are you sure you want to cancel and exit? <br />
+          If you cancel, your data won&apos;t be saved.
+        </Dialog.Description>
+        <Dialog.Actions>
+          <Button variant='primaryOutline' onClick={toggleDialog}>
+            No
+          </Button>
+          <LinkButton variant='primary' to='/admin/evaluations'>
+            Yes
+          </LinkButton>
+        </Dialog.Actions>
+      </Dialog>
+    </>
   )
 }
