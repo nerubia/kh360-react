7dd671b81eac6af68f21d2dd2bd9651f326ffc69
Emmalyn Acha <eacha@nerubia.com>
2023-11-06 17:08:58 +0800

    fix: requested changes

--- src/features/admin/evaluation-administrations/create/CreateEvaluationHeader.tsx
+++ src/features/admin/evaluation-administrations/create/CreateEvaluationHeader.tsx
@@ -1,5 +1,5 @@
 export const CreateEvaluationHeader = () => {
   return (
-    <h1 className='text-2xl text-primary-600 font-bold'>Create Evaluation</h1>
+    <h1 className='text-2xl text-primary-500 font-bold'>Create Evaluation</h1>
   )
 }
