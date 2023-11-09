e2fd7807c1e902054738b622ee0fa5d509977418
jatnerubia <jlerit@nerubia.com>
2023-11-09 11:41:02 +0800

    updated evaluators page layout

--- src/features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/EvaluatorsRoles.tsx
+++ src/features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/EvaluatorsRoles.tsx
@@ -1,12 +1,10 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { LinkButton } from "../../../../../../../components/button/Button"
-import { Icon } from "../../../../../../../components/icon/Icon"
 import { useEffect } from "react"
 import { getEvaluationTemplates } from "../../../../../../../redux/slices/evaluationTemplatesSlice"
 import { useAppSelector } from "../../../../../../../hooks/useAppSelector"
 import { useAppDispatch } from "../../../../../../../hooks/useAppDispatch"
 import { getEvaluationResult } from "../../../../../../../redux/slices/evaluationResultSlice"
-import { getEvaluationResultStatusColor } from "../../../../../../../utils/color"
 
 export const EvaluatorsRoles = () => {
   const navigate = useNavigate()
@@ -14,9 +12,6 @@ export const EvaluatorsRoles = () => {
   const appDispatch = useAppDispatch()
   const { evaluation_templates } = useAppSelector(
     (state) => state.evaluationTemplates
-  )
-  const { evaluation_result, previousId, nextId } = useAppSelector(
-    (state) => state.evaluationResult
   )
 
   useEffect(() => {
@@ -39,57 +34,7 @@ export const EvaluatorsRoles = () => {
   }, [evaluation_templates])
 
   return (
-    <div className='w-96 h-[calc(100vh_-_104px)] flex flex-col gap-4'>
-      <div className='flex items-center gap-4 rounded-md'>
-        <div className='w-6'>
-          {previousId !== undefined && (
-            <LinkButton
-              testId='PreviousButton'
-              variant='unstyled'
-              to={`/admin/evaluation-administrations/${id}/evaluees/${previousId}/evaluators/all`}
-            >
-              <Icon icon='ChevronLeft' />
-            </LinkButton>
-          )}
-        </div>
-        <div className='flex-1 flex items-center gap-4'>
-          {evaluation_result?.users?.picture === undefined ||
-          evaluation_result.users?.picture === null ? (
-            <Icon icon='UserFill' />
-          ) : (
-            <img
-              className='w-10 h-10 rounded-full'
-              src={evaluation_result?.users?.picture}
-              alt={`Avatar of ${evaluation_result?.users?.first_name} ${evaluation_result?.users?.first_name}`}
-            />
-          )}
-          <div className='flex-1'>
-            <p className='font-bold'>
-              {evaluation_result?.users?.last_name},{" "}
-              {evaluation_result?.users?.first_name}
-            </p>
-            <p
-              className={getEvaluationResultStatusColor(
-                evaluation_result?.status
-              )}
-            >
-              {evaluation_result?.status}
-            </p>
-          </div>
-        </div>
-        <div className='w-6'>
-          {nextId !== undefined && (
-            <LinkButton
-              testId='NextButton'
-              variant='unstyled'
-              to={`/admin/evaluation-administrations/${id}/evaluees/${nextId}/evaluators/all`}
-            >
-              <Icon icon='ChevronRight' />
-            </LinkButton>
-          )}
-        </div>
-      </div>
-      <h2 className='text-lg text-center font-bold'>Evaluators Roles</h2>
+    <div className='w-96 h-[calc(100vh_-_185px)] pt-4'>
       <div className='flex-1 flex flex-col gap-2 overflow-y-scroll'>
         {evaluation_templates.map((template) => (
           <LinkButton
--- src/pages/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators.tsx
+++ src/pages/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators.tsx
@@ -1,13 +1,18 @@
 import { Divider } from "../../../../../../../components/divider/Divider"
 import { EvaluatorsRoles } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/EvaluatorsRoles"
 import { EvaluatorsList } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/EvaluatorsList"
+import { EvaluatorsUser } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/EvaluatorsUser"
 
 export default function Evaluators() {
   return (
-    <div className='flex gap-4'>
-      <EvaluatorsRoles />
-      <Divider orientation='vertical' />
-      <EvaluatorsList />
+    <div className='flex flex-col'>
+      <EvaluatorsUser />
+      <Divider />
+      <div className='flex gap-4'>
+        <EvaluatorsRoles />
+        <Divider orientation='vertical' />
+        <EvaluatorsList />
+      </div>
     </div>
   )
 }
