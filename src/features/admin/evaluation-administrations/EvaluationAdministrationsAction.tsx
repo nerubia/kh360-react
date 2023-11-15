7dde2602cfa77599b9ef48e3abe151a2666171fb
Emmalyn Acha <eacha@nerubia.com>
2023-11-15 13:05:33 +0800

    fix: requested changes

--- src/features/admin/evaluation-administrations/EvaluationAdministrationsAction.tsx
+++ src/features/admin/evaluation-administrations/EvaluationAdministrationsAction.tsx
@@ -6,7 +6,7 @@ export const EvaluationAdministrationsAction = () => {
 
   return (
     <div className='flex flex-col items-center md:flex-row justify-between gap-4'>
-      <h2 className='text-gray-400 text-base'>
+      <h2 className='text-gray-400'>
         {totalItems} {totalItems === 1 ? "Result" : "Results"} Found
       </h2>
       <LinkButton to='/admin/evaluation-administrations/create'>Create Evaluations</LinkButton>
