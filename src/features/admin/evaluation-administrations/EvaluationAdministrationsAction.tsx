c0bcf95f4da1bc8641b8c2da80c1d2d82ade2e8f
Emmalyn Acha <eacha@nerubia.com>
2023-11-15 11:32:57 +0800

    fix: requested layout changes

--- src/features/admin/evaluation-administrations/EvaluationAdministrationsAction.tsx
+++ src/features/admin/evaluation-administrations/EvaluationAdministrationsAction.tsx
@@ -2,18 +2,14 @@ import { LinkButton } from "../../../components/button/Button"
 import { useAppSelector } from "../../../hooks/useAppSelector"
 
 export const EvaluationAdministrationsAction = () => {
-  const { totalItems } = useAppSelector(
-    (state) => state.evaluationAdministrations
-  )
+  const { totalItems } = useAppSelector((state) => state.evaluationAdministrations)
 
   return (
-    <div className='flex flex-col md:flex-row justify-between gap-4'>
-      <h2 className='text-gray-400 text-xl'>
+    <div className='flex flex-col items-center md:flex-row justify-between gap-4'>
+      <h2 className='text-gray-400 text-base'>
         {totalItems} {totalItems === 1 ? "Result" : "Results"} Found
       </h2>
-      <LinkButton to='/admin/evaluation-administrations/create'>
-        Create Evaluations
-      </LinkButton>
+      <LinkButton to='/admin/evaluation-administrations/create'>Create Evaluations</LinkButton>
     </div>
   )
 }
