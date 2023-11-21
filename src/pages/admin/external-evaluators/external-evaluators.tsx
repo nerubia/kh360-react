9774987a628bb83e516ffc5b27a56b4ca344261c
Emmalyn Acha <eacha@nerubia.com>
2023-11-21 13:18:53 +0800

    style: change filename format to kebab

--- src/pages/admin/external-evaluators/external-evaluators.tsx
+++ src/pages/admin/external-evaluators/external-evaluators.tsx
@@ -1,8 +1,8 @@
 import { useTitle } from "../../../hooks/useTitle"
-import { ExternalEvaluatorsHeader } from "../../../features/admin/external-evaluators/ExternalEvaluatorsHeader"
-import { ExternalEvaluatorsFilter } from "../../../features/admin/external-evaluators/ExternalEvaluatorsFilter"
-import { ExternalEvaluatorsTable } from "../../../features/admin/external-evaluators/ExternalEvaluatorsTable"
-import { ExternalEvaluatorsAction } from "../../../features/admin/external-evaluators/ExternalEvaluatorsAction"
+import { ExternalEvaluatorsHeader } from "../../../features/admin/external-evaluators/external-evaluators-header"
+import { ExternalEvaluatorsFilter } from "../../../features/admin/external-evaluators/external-evaluators-filter"
+import { ExternalEvaluatorsTable } from "../../../features/admin/external-evaluators/external-evaluators-table"
+import { ExternalEvaluatorsAction } from "../../../features/admin/external-evaluators/external-evaluators-action"
 
 export default function ExternalEvaluators() {
   useTitle("External Evaluators")
