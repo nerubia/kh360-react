3543ffdf974418fa97349fa714ebcf2383b3c9bf
jatnerubia <jlerit@nerubia.com>
2023-11-21 11:51:20 +0800

    updated sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import { logout } from "../../redux/slices/authSlice"
 import { Icon } from "../icon/Icon"
 import { Button } from "../button/Button"
 import { Menu } from "../../components/shared/Menu"
+import { type icons } from "../icon/icons"
 
 export const Sidebar = () => {
   const { activeSidebar } = useAppSelector((state) => state.app)
@@ -15,20 +16,28 @@ export const Sidebar = () => {
 
   const MenuLinks = [
     {
-      title: "Dashboard",
-      link: "/dashboard",
-    },
-    {
       title: "Sample",
       link: "/sample",
     },
     {
-      title: "Evaluations",
-      link: "/evaluation-administrations",
+      title: "Dashboard",
+      link: "/dashboard",
+      icon: "Dashboard",
     },
     {
-      title: "Admin",
+      title: "Evaluations",
+      link: "/evaluation-administrations",
+      icon: "Star",
+    },
+    {
+      title: "Evaluation Admin",
       link: "/admin/evaluation-administrations",
+      icon: "PenSquare",
+    },
+    {
+      title: "External Evaluators",
+      link: "/admin/external-evaluators",
+      icon: "UserFill",
     },
   ]
 
@@ -65,16 +74,17 @@ export const Sidebar = () => {
               to={menu.link}
               isEvaluation={false}
               isAdmin={isAdmin}
-              className='w-fit rounded-md flex items-center gap-2 bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default h-9 text-base px-4 !w-full'
+              className='w-full rounded-md flex items-center gap-2 bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default h-9 text-base px-4'
             >
+              {menu.icon != null && <Icon icon={menu.icon as keyof typeof icons} />}
               {menu.title}
             </Menu>
           ))}
+          <Button fullWidth center={false} onClick={handleLogout}>
+            <Icon icon='Logout' />
+            Logout
+          </Button>
         </div>
-        <Button fullWidth onClick={handleLogout}>
-          <Icon icon='Logout' />
-          Logout
-        </Button>
       </div>
     </div>
   )
