5ce4982c4176d6990d9dddf597ad060a4ad6e5bb
Emmalyn Acha <eacha@nerubia.com>
2023-12-07 18:35:03 +0800

    fix: update evaluation form

--- src/pages/evaluations/[id]/evaluations.tsx
+++ src/pages/evaluations/[id]/evaluations.tsx
@@ -7,7 +7,7 @@ export default function Evaluations() {
   return (
     <>
       <EvaluationsHeader />
-      <div className='h-[calc(100vh_-_180px)] flex flex-col md:flex-row md:w-11/12 shadow-md'>
+      <div className='h-[calc(100vh_-_180px)] flex flex-col md:flex-row md:w-[95%] shadow-md'>
         <EvaluationsList />
         <Divider orientation='vertical' />
         <EvaluationsCriteria />
