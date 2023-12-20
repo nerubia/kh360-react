c3043833cd4de81b119887497cf3ab47b1de278c
jatnerubia <jlerit@nerubia.com>
2023-12-20 16:25:29 +0800

    added title

--- src/pages/admin/evaluation-administrations/[id]/select/select-evaluees.tsx
+++ src/pages/admin/evaluation-administrations/[id]/select/select-evaluees.tsx
@@ -2,8 +2,11 @@ import { SelectEvalueesHeader } from "../../../../../features/admin/evaluation-a
 import { SelectEvalueesTable } from "../../../../../features/admin/evaluation-administrations/[id]/select/select-evaluees-table"
 import { SelectEvalueesFilter } from "../../../../../features/admin/evaluation-administrations/[id]/select/select-evaluees-filter"
 import { SelectEvalueesFooter } from "../../../../../features/admin/evaluation-administrations/[id]/select/select-evaluees-footer"
+import { useTitle } from "../../../../../hooks/useTitle"
 
 export default function SelectEvaluees() {
+  useTitle("Select Evaluees")
+
   return (
     <div className='h-[calc(100vh_-_104px)] flex flex-col gap-8'>
       <SelectEvalueesHeader />
