1a4da8655e3da3b6fff2431615a7cb0ea9656e7d
jatnerubia <jlerit@nerubia.com>
2023-12-20 08:33:50 +0800

    refactor: routes

--- src/routes/AdminRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useAdmin } from "../hooks/useAdmin"
-
-export default function AdminRoute() {
-  const isAdmin = useAdmin()
-  return isAdmin ? <Outlet /> : <Navigate to='/dashboard' />
-}
