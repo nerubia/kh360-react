23812be816ea9b58c2ca6cb10e671e55fb143c16
jatnerubia <jlerit@nerubia.com>
2023-12-20 08:33:50 +0800

    refactor: routes

---
+++ src/routes/guest-route.tsx
@@ -0,0 +1,22 @@
+import { lazy } from "react"
+
+const Layout = lazy(async () => await import("../components/layouts/Layout"))
+
+const Home = lazy(async () => await import("../pages/home"))
+const ExternalEvaluations = lazy(
+  async () => await import("../pages/external-evaluations/external-evaluations")
+)
+
+export const guestRoutes = {
+  element: <Layout />,
+  children: [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/external-evaluations/:id/evaluations/:evaluation_id",
+      element: <ExternalEvaluations />,
+    },
+  ],
+}
