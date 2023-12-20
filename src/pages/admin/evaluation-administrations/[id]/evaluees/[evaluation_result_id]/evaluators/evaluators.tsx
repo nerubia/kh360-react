7ccb77785c07d1e8c25775a0f34ac7ac45e90bd9
jatnerubia <jlerit@nerubia.com>
2023-12-20 16:25:29 +0800

    added title

--- src/pages/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators.tsx
+++ src/pages/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators.tsx
@@ -2,8 +2,11 @@ import { Divider } from "../../../../../../../components/ui/divider/divider"
 import { EvaluatorsRoles } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators-roles"
 import { EvaluatorsList } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators-list"
 import { EvaluatorsUser } from "../../../../../../../features/admin/evaluation-administrations/[id]/evaluees/[evaluation_result_id]/evaluators/evaluators-user"
+import { useTitle } from "../../../../../../../hooks/useTitle"
 
 export default function Evaluators() {
+  useTitle("Evaluators")
+
   return (
     <div className='flex flex-col'>
       <EvaluatorsUser />
