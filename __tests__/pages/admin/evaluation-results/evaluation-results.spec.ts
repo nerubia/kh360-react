09921893319ea656d92fbcd9095c9597bedd3ea9
emmaacha <ecacha@up.edu.ph>
2023-12-20 15:04:30 +0800

    update route for evaluation results page

--- __tests__/pages/admin/evaluation-results/evaluation-results.spec.ts
+++ __tests__/pages/admin/evaluation-results/evaluation-results.spec.ts
@@ -5,7 +5,7 @@ import { loginUser } from "../../../utils/login-user"
 
 setupPlaywright()
 
-test.describe("Admin - Evaluation results", () => {
+test.describe("Bod - Evaluation results", () => {
   test.beforeEach(async ({ page }) => {
     await mockRequest(page, "/auth/refresh", {
       status: 403,
@@ -19,30 +19,89 @@ test.describe("Admin - Evaluation results", () => {
   })
 
   test.describe("as Guest", () => {
-    test("should not allow to view the admin evaluation results", async ({ page }) => {
-      await page.goto("/admin/evaluation-results")
+    test("should not allow to view the evaluation results", async ({ page }) => {
+      await page.goto("/evaluation-results")
 
-      await expect(page).toHaveURL("/auth/login?callback=/admin/evaluation-results")
+      await expect(page).toHaveURL("/auth/login?callback=/evaluation-results")
     })
   })
 
   test.describe("as Employee", () => {
-    test("should not allow to view the admin evaluation results", async ({ page }) => {
+    test("should not allow to view the evaluation results", async ({ page }) => {
       await loginUser("employee", page)
 
-      await page.goto("/admin/evaluation-results")
+      await page.goto("/evaluation-results")
 
-      await expect(page).toHaveURL("/dashboard")
+      await mockRequest(page, "/user/evaluation-administrations", {
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify({
+          data: [
+            {
+              id: 1,
+              name: "Evaluation 1",
+              eval_schedule_start_date: "2024-04-06T00:00:00.000Z",
+              eval_schedule_end_date: "2023-03-14T00:00:00.000Z",
+              eval_period_start_date: "2023-07-22T00:00:00.000Z",
+              eval_period_end_date: "2023-09-22T00:00:00.000Z",
+              remarks: "This is description",
+              status: "ongoing",
+              totalEvaluations: 3,
+              totalSubmitted: 0,
+              totalPending: 3,
+            },
+            {
+              id: 2,
+              name: "Evaluation 2",
+              eval_schedule_start_date: "2024-04-06T00:00:00.000Z",
+              eval_schedule_end_date: "2023-03-14T00:00:00.000Z",
+              eval_period_start_date: "2023-07-22T00:00:00.000Z",
+              eval_period_end_date: "2023-09-22T00:00:00.000Z",
+              remarks: "This is description",
+              status: "ongoing",
+              totalEvaluations: 3,
+              totalSubmitted: 1,
+              totalPending: 2,
+            },
+            {
+              id: 3,
+              name: "Evaluation 3",
+              eval_schedule_start_date: "2024-04-06T00:00:00.000Z",
+              eval_schedule_end_date: "2023-03-14T00:00:00.000Z",
+              eval_period_start_date: "2023-07-22T00:00:00.000Z",
+              eval_period_end_date: "2023-09-22T00:00:00.000Z",
+              remarks: "This is description",
+              status: "ongoing",
+              totalEvaluations: 4,
+              totalSubmitted: 2,
+              totalPending: 2,
+            },
+          ],
+          pageInfo: {
+            hasPreviousPage: false,
+            hasNextPage: false,
+            totalPages: 1,
+          },
+        }),
+      })
+
+      await mockRequest(page, "/user/email-templates?template_type=No+Pending+Evaluation+Forms", {
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify({}),
+      })
+
+      await expect(page).toHaveURL("/evaluation-administrations")
     })
   })
 
-  test.describe("as Admin", () => {
+  test.describe("as Bod", () => {
     test("should render correctly", async ({ page }) => {
-      await loginUser("admin", page)
+      await loginUser("bod", page)
 
-      await page.goto("/admin/evaluation-results")
+      await page.goto("/evaluation-results")
 
-      await mockRequest(page, "/admin/evaluation-administrations?status=Closed,Published", {
+      await mockRequest(page, "/bod/evaluation-administrations?status=Closed,Published", {
         status: 200,
         contentType: "application/json",
         body: JSON.stringify({
--- __tests__/utils/login-user.ts
+++ __tests__/utils/login-user.ts
@@ -1,7 +1,7 @@
 import { type Page } from "@playwright/test"
 import { mockRequest } from "./mock-request"
 
-export const loginUser = async (user: "employee" | "admin", page: Page) => {
+export const loginUser = async (user: "employee" | "admin" | "bod", page: Page) => {
   if (user === "employee") {
     await mockRequest(page, "/auth/refresh", {
       status: 200,
@@ -30,6 +30,25 @@ export const loginUser = async (user: "employee" | "admin", page: Page) => {
           first_name: "Admin",
           last_name: "User",
           roles: ["kh360"],
+        },
+      }),
+    })
+  }
+  if (user === "bod") {
+    await mockRequest(page, "/auth/refresh", {
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify({
+        access_token: "some access token",
+        user: {
+          id: 1,
+          email: "admin@nerubia.com",
+          first_name: "Admin",
+          last_name: "User",
+          roles: [],
+          user_details: {
+            user_type: "bod",
+          },
         },
       }),
     })
