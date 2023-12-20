364c5b0285f84eaa39feb89a67f1bbcd5aa4580c
jatnerubia <jlerit@nerubia.com>
2023-12-20 14:32:25 +0800

    updated menu size

--- src/components/shared/sidebar/sidebar.tsx
+++ src/components/shared/sidebar/sidebar.tsx
@@ -135,7 +135,7 @@ export const Sidebar = () => {
                   <Menu
                     to={menu.link}
                     isEvaluation={false}
-                    className={`w-full rounded-md flex justify-between items-center bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default h-9 text-base px-4 ${
+                    className={`w-full rounded-md flex justify-between items-center bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default h-9 px-4 ${
                       isParentActive(menu) ? "!bg-primary-700" : ""
                     }`}
                   >
@@ -151,7 +151,7 @@ export const Sidebar = () => {
                         <Menu
                           to={child.link}
                           isEvaluation={false}
-                          className='w-full rounded-md flex items-center gap-2 bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default px-4 py-2'
+                          className='w-full rounded-md flex items-center gap-2 bg-primary-500 text-sm text-white hover:bg-primary-600 active:bg-primary-700 disabled:bg-primary-200 [&.active]:bg-primary-700 [&.active]:cursor-default px-4 py-2'
                         >
                           {child.icon != null && <Icon icon={child.icon as keyof typeof icons} />}
                           {child.title}
