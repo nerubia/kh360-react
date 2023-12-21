88601a9a5e2e6fef1ca29e7ae5dabaff0a208ec5
jatnerubia <jlerit@nerubia.com>
2023-12-21 16:31:22 +0800

    updated eval results page

---
+++ src/features/evaluation-results/evaluation-results-header.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from "../../components/shared/page-title"
+
+export const EvaluationResultsListHeader = () => {
+  return (
+    <div className='flex flex-col md:flex-row justify-between gap-4'>
+      <PageTitle>Evaluation Results</PageTitle>
+    </div>
+  )
+}
