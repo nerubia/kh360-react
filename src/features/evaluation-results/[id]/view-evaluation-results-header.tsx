941b4700ba0451d6841e4f88ae3c229b5b80feb1
emmaacha <emmalyncayuna@gmail.com>
2023-12-21 17:33:39 +0800

    feat: add evaluation results page

--- src/features/evaluation-results/[id]/view-evaluation-results-header.tsx
+++ src/features/evaluation-results/[id]/view-evaluation-results-header.tsx
@@ -4,17 +4,17 @@ import { useParams } from "react-router-dom"
 import { formatDateRange } from "../../../utils/format-date"
 import { useAppDispatch } from "../../../hooks/useAppDispatch"
 import { PageTitle } from "../../../components/shared/page-title"
-import { getUserEvaluationResult } from "../../../redux/slices/user-slice"
+import { getEvaluationResult } from "../../../redux/slices/evaluation-result-slice"
 import { ScoreRange } from "../../../components/shared/score-range/score-range"
 
-export const MyEvaluationResultsHeader = () => {
+export const ViewEvaluationResultsHeader = () => {
   const { id } = useParams()
   const appDispatch = useAppDispatch()
-  const { user_evaluation_result } = useAppSelector((state) => state.user)
+  const { evaluation_result } = useAppSelector((state) => state.evaluationResult)
 
   useEffect(() => {
     if (id !== undefined) {
-      void appDispatch(getUserEvaluationResult(parseInt(id)))
+      void appDispatch(getEvaluationResult(parseInt(id)))
     }
   }, [])
 
@@ -27,29 +27,28 @@ export const MyEvaluationResultsHeader = () => {
               <PageTitle>Evaluation Results</PageTitle>
             </div>
             <div className='flex gap-3 font-bold'>
-              {user_evaluation_result?.users?.last_name},{" "}
-              {user_evaluation_result?.users?.first_name}
+              {evaluation_result?.users?.last_name}, {evaluation_result?.users?.first_name}
             </div>
-            <div className='flex gap-3'>{user_evaluation_result?.eval_admin_name}</div>
+            <div className='flex gap-3'>{evaluation_result?.eval_admin_name}</div>
             <div className='flex gap-3'>
               {formatDateRange(
-                user_evaluation_result?.eval_period_start_date,
-                user_evaluation_result?.eval_period_end_date
+                evaluation_result?.eval_period_start_date,
+                evaluation_result?.eval_period_end_date
               )}
             </div>
           </div>
         </div>
         <div className='text-xl text-primary-500 font-bold my-5'>
-          Total Score: {user_evaluation_result?.total_score}%
+          Total Score: {evaluation_result?.total_score}%
         </div>
-        {user_evaluation_result?.score_rating !== undefined &&
-          user_evaluation_result?.users?.picture !== undefined && (
+        {evaluation_result?.score_rating !== undefined &&
+          evaluation_result?.users?.picture !== undefined && (
             <ScoreRange
-              user_picture={user_evaluation_result?.users?.picture}
-              score_rating={user_evaluation_result?.score_rating}
-              score={user_evaluation_result?.score}
+              user_picture={evaluation_result?.users?.picture}
+              score_rating={evaluation_result?.score_rating}
+              score={evaluation_result?.score}
               size='medium'
-              is_evaluee={true}
+              is_evaluee={false}
             />
           )}
       </div>
