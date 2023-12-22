58405410e2f3d9e9e2e8cc2a849abf8b2ed6ae0c
jatnerubia <jlerit@nerubia.com>
2023-12-22 16:31:09 +0800

    added attendance and punctuality info

--- src/pages/evaluation-results/[id]/view-evaluation-results.tsx
+++ src/pages/evaluation-results/[id]/view-evaluation-results.tsx
@@ -2,6 +2,7 @@ import { ViewEvaluationResultsHeader } from "../../../features/evaluation-result
 import { ViewEvaluationResultsTable } from "../../../features/evaluation-results/[id]/view-evaluation-results-table"
 import { ViewEvaluationResultsComments } from "../../../features/evaluation-results/[id]/view-evaluation-results-comments"
 import { ViewEvaluationResultsFooter } from "../../../features/evaluation-results/[id]/view-evaluation-results-footer"
+import { ViewEvaluationResultsAttendanceAndPunctuality } from "../../../features/evaluation-results/[id]/view-evaluation-results-attendance-and-punctuality"
 
 export default function ViewEvaluationResults() {
   return (
@@ -9,6 +10,7 @@ export default function ViewEvaluationResults() {
       <div className='h-[calc(100vh_-_104px)] flex flex-col gap-8'>
         <ViewEvaluationResultsHeader />
         <ViewEvaluationResultsTable />
+        <ViewEvaluationResultsAttendanceAndPunctuality />
         <ViewEvaluationResultsComments />
         <ViewEvaluationResultsFooter />
       </div>
