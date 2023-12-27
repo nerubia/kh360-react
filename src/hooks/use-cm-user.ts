52a237f9325e1735ec377e5db1f5c159d4b249d1
emmaacha <emmalyncayuna@gmail.com>
2023-12-27 16:09:36 +0800

    fix evaluation result permissions

--- src/hooks/use-cm-user.ts
+++ src/hooks/use-cm-user.ts
@@ -2,5 +2,9 @@ import { useAppSelector } from "./useAppSelector"
 
 export const useCmUser = () => {
   const { user } = useAppSelector((state) => state.auth)
-  return user?.roles?.includes("khv2_cm_admin") ?? user?.roles?.includes("khv2_cm") ?? false
+  return (
+    user?.roles?.includes("khv2_cm_admin") === true ||
+    user?.roles?.includes("khv2_cm") === true ||
+    false
+  )
 }
