e658c3d30440f93840d341185ca076e26bca386c
Niño Allen Ardiente <ardientenino@gmail.com>
2024-01-04 17:51:50 +0800

    implemented websockets

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -18,6 +18,7 @@ import projectsReducer from "./slices/projects-slice"
 import scoreRatingsReducer from "./slices/score-ratings-slice"
 import usersReducer from "./slices/users-slice"
 import userReducer from "./slices/user-slice"
+import websocketReducer from "./slices/websocket-slice"
 
 export const store = configureStore({
   reducer: {
@@ -39,6 +40,7 @@ export const store = configureStore({
     scoreRatings: scoreRatingsReducer,
     users: usersReducer,
     user: userReducer,
+    websocket: websocketReducer,
   },
 })
 
