636ec40df97e3115a516c1d9a93475870c742c50
Niño Allen Ardiente <ardientenino@gmail.com>
2024-01-05 11:27:16 +0800

    revised setup websocket

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Provider } from "react-redux"
 import "./App.css"
 import { store } from "./redux/store"
 import { setupAxiosInstance } from "./utils/axios-instance"
-import { setupWebsocket } from "./utils/websocket"
 import { PersistLogin } from "./features/persist-login"
 
 function App() {
   setupAxiosInstance(store)
-  setupWebsocket(store)
   return (
     <Provider store={store}>
       <PersistLogin />
