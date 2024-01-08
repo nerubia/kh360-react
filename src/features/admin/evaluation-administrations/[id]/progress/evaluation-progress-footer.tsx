e1ab50e56a8893347d0a6e0b6891b1a34f68faa0
Chadie Gil <caugis@nerubia.com>
2024-01-08 13:28:57 +0800

    kh360-react-443-progress-screen-mobile-view

--- src/features/admin/evaluation-administrations/[id]/progress/evaluation-progress-footer.tsx
+++ src/features/admin/evaluation-administrations/[id]/progress/evaluation-progress-footer.tsx
@@ -1,18 +1,23 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { Button } from "../../../../../components/ui/button/button"
 import { Icon } from "../../../../../components/ui/icon/icon"
-
+import useMobileView from "../../../../../hooks/use-mobile-view"
 export const EvaluationProgressFooter = () => {
   const navigate = useNavigate()
   const { id } = useParams()
-
+  const isMobile = useMobileView()
   const handleGoBack = () => {
     navigate(`/admin/evaluation-administrations/${id}`)
   }
 
   return (
     <>
-      <Button testId='BackButton' variant='primaryOutline' size='medium' onClick={handleGoBack}>
+      <Button
+        testId='BackButton'
+        variant='primaryOutline'
+        size={isMobile ? "small" : "medium"}
+        onClick={handleGoBack}
+      >
         <Icon icon='ChevronLeft' />
       </Button>
     </>
