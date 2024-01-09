7310c1d0655fdfa32efc08d66b81e2e4b4dc4da7
jatnerubia <jlerit@nerubia.com>
2024-01-09 10:53:20 +0800

    added projects menu

---
+++ src/components/ui/icon/icons/target-icon.tsx
@@ -0,0 +1,19 @@
+export default function TargetIcon({ size }: { size: number }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={size}
+      height={size}
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <circle cx='12' cy='12' r='10' />
+      <circle cx='12' cy='12' r='6' />
+      <circle cx='12' cy='12' r='2' />
+    </svg>
+  )
+}
