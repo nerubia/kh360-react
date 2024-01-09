4dc3d9f68ffa6c20358b4b9ad7f24b5983b3b039
Chadie Gil <caugis@nerubia.com>
2024-01-09 13:56:13 +0800

    refactor/fix useMobile hook eslint error

--- src/features/my-evaluations/[id]/my-evaluation-results-footer.tsx
+++ src/features/my-evaluations/[id]/my-evaluation-results-footer.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom"
 import { Button } from "../../../components/ui/button/button"
 import { Icon } from "../../../components/ui/icon/icon"
-import useMobileView from "../../../hooks/use-mobile-view"
+import { useMobileView } from "../../../hooks/use-mobile-view"
 
 export const MyEvaluationResultsFooter = () => {
   const navigate = useNavigate()
   const isMobile = useMobileView()
+
+  const size = isMobile ? "small" : "medium"
 
   const handleGoBack = () => {
     navigate(`/my-evaluations`)
@@ -13,12 +15,7 @@ export const MyEvaluationResultsFooter = () => {
 
   return (
     <div className='pb-5'>
-      <Button
-        testId='BackButton'
-        variant='primaryOutline'
-        size={isMobile ? "small" : "medium"}
-        onClick={handleGoBack}
-      >
+      <Button testId='BackButton' variant='primaryOutline' size={size} onClick={handleGoBack}>
         <Icon icon='ChevronLeft' />
       </Button>
     </div>
