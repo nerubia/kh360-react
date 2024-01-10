83d401c711dfdb0d9342cb0da7070306e70d938f
Niño Allen Ardiente <ardientenino@gmail.com>
2024-01-10 14:31:27 +0800

    updated create evaluation template screen

--- src/features/admin/evaluation-templates/create/create-evaluation-template-header.tsx
+++
@@ -1,5 +0,0 @@
-import { PageTitle } from "../../../../components/shared/page-title"
-
-export const CreateEvaluationTemplateHeader = () => {
-  return <PageTitle>Add Evaluation Template</PageTitle>
-}
