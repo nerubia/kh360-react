877096c42d4317e8b11ebdceeee35f48c1774dc6
Chadie Gil <caugis@nerubia.com>
2024-01-10 18:51:14 +0800

    kh360-react-447-eval-result-screen-mobile-view

--- src/features/evaluation-results/[id]/view-evaluation-results-attendance-and-punctuality.tsx
+++ src/features/evaluation-results/[id]/view-evaluation-results-attendance-and-punctuality.tsx
@@ -18,96 +18,98 @@ export const ViewEvaluationResultsAttendanceAndPunctuality = () => {
       {loading === Loading.Fulfilled && (
         <div>
           <Button variant='unstyled' onClick={toggleTable}>
-            <div className='flex gap-2 text-xl font-bold mb-2 text-primary-500'>
+            <div className='flex gap-2 text-base md:text-xl font-bold mb-2 text-primary-500'>
               Attendance and Punctuality
               {showTable ? <Icon icon='ChevronDown' /> : <Icon icon='ChevronUp' />}{" "}
             </div>
           </Button>
           {showTable && (
-            <table className='w-full table-fixed'>
-              <thead className='text-left'>
-                <tr className='text-center'>
-                  <th className='border-b-4 text-start'>Month</th>
-                  <th className='border-b-4'># Working Days</th>
-                  <th className='border-b-4'>
-                    # of Days
-                    <br />
-                    Present
-                  </th>
-                  <th className='border-b-4'>
-                    # of Lates
-                    <br />
-                    (GC)
-                  </th>
-                  <th className='border-b-4'>
-                    # of Lates
-                    <br />
-                    (Counted)
-                  </th>
-                  <th className='border-b-4'>
-                    # of
-                    <br />
-                    VL/BL
-                  </th>
-                  <th className='border-b-4'>
-                    # of
-                    <br />
-                    SL
-                  </th>
-                  <th className='border-b-4'>
-                    # of
-                    <br />
-                    EL
-                  </th>
-                  <th className='border-b-4'>
-                    # of Other
-                    <br />
-                    Leaves
-                  </th>
-                  <th className='border-b-4'>
-                    # of Unpaid
-                    <br />
-                    Leaves
-                  </th>
-                  <th className='border-b-4'>
-                    # of Unfiled
-                    <br />
-                    Leaves
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {evaluation_result?.attendance_and_punctuality?.map((data, index) => (
-                  <tr className='text-center hover:bg-slate-100' key={index}>
-                    <td className='py-1 text-start border-b'>{data.month}</td>
-                    <td className='py-1 border-b'>{data.total_working_days}</td>
-                    <td className='py-1 border-b'>{data.days_present}</td>
-                    <td
-                      className={`py-1 border-b ${
-                        data.lates_grace_period !== undefined && data.lates_grace_period > 0
-                          ? "text-red-500"
-                          : ""
-                      }`}
-                    >
-                      {data.lates_grace_period}
-                    </td>
-                    <td
-                      className={`py-1 border-b ${
-                        data.lates !== undefined && data.lates > 0 ? "text-red-500" : ""
-                      }`}
-                    >
-                      {data.lates}
-                    </td>
-                    <td className='py-1 border-b'>{data.vacation_and_birthday_leave_duration}</td>
-                    <td className='py-1 border-b'>{data.sick_leave_duration}</td>
-                    <td className='py-1 border-b'>{data.emergency_leave_duration}</td>
-                    <td className='py-1 border-b'>{data.other_leave_duration}</td>
-                    <td className='py-1 border-b'>{data.unpaid_leave_duration}</td>
-                    <td className='py-1 border-b'>{data.unfiled_leave_duration}</td>
+            <div className='overflow-x-auto'>
+              <table className='w-full table-fixed whitespace-nowrap'>
+                <thead className='text-left'>
+                  <tr className='text-center'>
+                    <th className='border-b-4 px-4 w-[100px] text-start'>Month</th>
+                    <th className='border-b-4 px-4 w-[125px]'># Working Days</th>
+                    <th className='border-b-4 p-6 w-[100px]'>
+                      # of Days
+                      <br />
+                      Present
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of Lates
+                      <br />
+                      (GC)
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of Lates
+                      <br />
+                      (Counted)
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of
+                      <br />
+                      VL/BL
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of
+                      <br />
+                      SL
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of
+                      <br />
+                      EL
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of Other
+                      <br />
+                      Leaves
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of Unpaid
+                      <br />
+                      Leaves
+                    </th>
+                    <th className='border-b-4 px-4 w-[100px]'>
+                      # of Unfiled
+                      <br />
+                      Leaves
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {evaluation_result?.attendance_and_punctuality?.map((data, index) => (
+                    <tr className='text-center hover:bg-slate-100' key={index}>
+                      <td className='py-1 text-start border-b'>{data.month}</td>
+                      <td className='py-1 border-b'>{data.total_working_days}</td>
+                      <td className='py-1 border-b'>{data.days_present}</td>
+                      <td
+                        className={`py-1 border-b ${
+                          data.lates_grace_period !== undefined && data.lates_grace_period > 0
+                            ? "text-red-500"
+                            : ""
+                        }`}
+                      >
+                        {data.lates_grace_period}
+                      </td>
+                      <td
+                        className={`py-1 border-b ${
+                          data.lates !== undefined && data.lates > 0 ? "text-red-500" : ""
+                        }`}
+                      >
+                        {data.lates}
+                      </td>
+                      <td className='py-1 border-b'>{data.vacation_and_birthday_leave_duration}</td>
+                      <td className='py-1 border-b'>{data.sick_leave_duration}</td>
+                      <td className='py-1 border-b'>{data.emergency_leave_duration}</td>
+                      <td className='py-1 border-b'>{data.other_leave_duration}</td>
+                      <td className='py-1 border-b'>{data.unpaid_leave_duration}</td>
+                      <td className='py-1 border-b'>{data.unfiled_leave_duration}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           )}
         </div>
       )}
--- src/features/evaluation-results/[id]/view-evaluation-results-footer.tsx
+++ src/features/evaluation-results/[id]/view-evaluation-results-footer.tsx
@@ -2,11 +2,12 @@ import { useNavigate } from "react-router-dom"
 import { Button } from "../../../components/ui/button/button"
 import { Icon } from "../../../components/ui/icon/icon"
 import { useAppSelector } from "../../../hooks/useAppSelector"
+import { useMobileView } from "../../../hooks/use-mobile-view"
 
 export const ViewEvaluationResultsFooter = () => {
   const navigate = useNavigate()
   const { previousUrl } = useAppSelector((state) => state.app)
-
+  const isMobile = useMobileView()
   const handleGoBack = () => {
     if (previousUrl !== null) {
       navigate(previousUrl)
@@ -17,7 +18,12 @@ export const ViewEvaluationResultsFooter = () => {
 
   return (
     <div className='pb-5'>
-      <Button testId='BackButton' variant='primaryOutline' size='medium' onClick={handleGoBack}>
+      <Button
+        testId='BackButton'
+        variant='primaryOutline'
+        size={isMobile ? "small" : "medium"}
+        onClick={handleGoBack}
+      >
         <Icon icon='ChevronLeft' />
       </Button>
     </div>
