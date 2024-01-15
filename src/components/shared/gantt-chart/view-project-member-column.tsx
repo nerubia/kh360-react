718dae835f0fc5909dfe9e1ad68ee2663fb9a3fe
emmaacha <emmalynacha@gmail.com>
2024-01-15 13:09:19 +0800

    feat: add view project screen

---
+++ src/components/shared/gantt-chart/view-project-member-column.tsx
@@ -0,0 +1,60 @@
+import { type Task } from "custom-gantt-task-react"
+import React from "react"
+import { Icon } from "../../ui/icon/icon"
+import Tooltip from "../../ui/tooltip/tooltip"
+
+export const ViewProjectMemberColumn: React.FC<{
+  rowHeight: number
+  rowWidth: string
+  fontFamily: string
+  fontSize: string
+  locale: string
+  tasks: Task[]
+  selectedTaskId: string
+  setSelectedTask: (taskId: string) => void
+  onExpanderClick: (task: Task) => void
+}> = ({ tasks, rowWidth }) => {
+  return (
+    <div className='table border-l md:w-[450px] w-full'>
+      {tasks.map((t, index) => {
+        return (
+          <div
+            className={`table-row text-ellipsis ${index % 2 === 0 ? "" : "bg-[#f5f5f5]"}`}
+            key={`${t.id}row`}
+          >
+            <div className='table-cell vertical-align h-[50px] md:w-[200px] ' title={t.name}>
+              <div className='p-2'>
+                <div>{t.name}</div>
+              </div>
+            </div>
+            <div
+              style={{
+                minWidth: rowWidth,
+                maxWidth: rowWidth,
+              }}
+              className='table-cell align-middle md:w-[250px] overflow-visible'
+            >
+              {(t?.dependencies?.length ?? 0) > 0 && (
+                <Tooltip placement='right'>
+                  <Tooltip.Trigger>
+                    <Icon icon='Info' color={"primary"}></Icon>
+                  </Tooltip.Trigger>
+                  <Tooltip.Content>
+                    {t?.dependencies?.map((projectMemberSkill, index) => (
+                      <pre
+                        className='flex font-sans break-words whitespace-pre-wrap text-xs md:text-xs md:w-[150px]'
+                        key={index}
+                      >
+                        {projectMemberSkill}
+                      </pre>
+                    ))}
+                  </Tooltip.Content>
+                </Tooltip>
+              )}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
--- src/types/project-type.ts
+++ src/types/project-type.ts
@@ -1,4 +1,6 @@
 import { type Client } from "./client-type"
+import { type Skill } from "./skill-type"
+import { type ProjectMember } from "./project-member-type"
 
 export interface Project {
   id: number
@@ -6,6 +8,10 @@ export interface Project {
   status?: string
   description?: string
   client?: Client
+  start_date?: string
+  end_date?: string
+  project_skills?: Skill[]
+  project_members?: ProjectMember[]
 }
 
 export interface ProjectFilters {
