5bd63ea6804aaa8e69c9c2efed18ea689293743c
emmaacha <emmalynacha@gmail.com>
2024-02-02 09:49:24 +0800

    update select evaluees footer dialogs

--- src/features/admin/evaluation-administrations/[id]/select/select-evaluees-footer.tsx
+++ src/features/admin/evaluation-administrations/[id]/select/select-evaluees-footer.tsx
@@ -2,10 +2,10 @@ import { useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { Button } from "@components/ui/button/button"
 import { Icon } from "@components/ui/icon/icon"
-import Dialog from "@components/ui/dialog/dialog"
 import { setSelectedEmployeeIds } from "@redux/slices/evaluation-administration-slice"
 import { useAppDispatch } from "@hooks/useAppDispatch"
 import { useAppSelector } from "@hooks/useAppSelector"
+import { CustomDialog } from "@components/ui/dialog/custom-dialog"
 
 export const SelectEvalueesFooter = () => {
   const { id } = useParams()
@@ -58,36 +58,30 @@ export const SelectEvalueesFooter = () => {
           </Button>
         </div>
       </div>
-      <Dialog open={showCancelDialog}>
-        <Dialog.Title>Cancel & Exit</Dialog.Title>
-        <Dialog.Description>
-          Are you sure you want to cancel and exit? <br />
-          If you cancel, your data won&apos;t be saved.
-        </Dialog.Description>
-        <Dialog.Actions>
-          <Button variant='primaryOutline' onClick={toggleCancelDialog}>
-            No
-          </Button>
-          <Button variant='primary' onClick={handleCancelAndExit}>
-            Yes
-          </Button>
-        </Dialog.Actions>
-      </Dialog>
-      <Dialog open={showBackDialog}>
-        <Dialog.Title>Go Back</Dialog.Title>
-        <Dialog.Description>
-          Are you sure you want to go back? <br />
-          If you go back, your data won&apos;t be saved.
-        </Dialog.Description>
-        <Dialog.Actions>
-          <Button variant='primaryOutline' onClick={toggleBackDialog}>
-            No
-          </Button>
-          <Button variant='primary' onClick={handleGoBack}>
-            Yes
-          </Button>
-        </Dialog.Actions>
-      </Dialog>
+      <CustomDialog
+        open={showCancelDialog}
+        title='Cancel & Exit'
+        description={
+          <>
+            Are you sure you want to cancel and exit? <br />
+            If you cancel, your data won&apos;t be saved.
+          </>
+        }
+        onClose={toggleCancelDialog}
+        onSubmit={handleCancelAndExit}
+      />
+      <CustomDialog
+        open={showBackDialog}
+        title='Go Back'
+        description={
+          <>
+            Are you sure you want to go back? <br />
+            If you go back, your data won&apos;t be saved.
+          </>
+        }
+        onClose={toggleBackDialog}
+        onSubmit={handleGoBack}
+      />
     </>
   )
 }
