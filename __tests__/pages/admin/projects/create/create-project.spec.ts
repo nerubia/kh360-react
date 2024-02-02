f8a1d3920021b565f7ff6ebe186fe8e7ce1eac04
emmaacha <emmalynacha@gmail.com>
2024-02-02 10:58:58 +0800

    update project form dialogs

--- __tests__/pages/admin/projects/create/create-project.spec.ts
+++ __tests__/pages/admin/projects/create/create-project.spec.ts
@@ -251,7 +251,7 @@ test.describe("Admin - Create Project", () => {
         page.getByText("Are you sure you want to cancel? If you cancel, your data won't be saved")
       ).toBeVisible()
       await expect(page.getByRole("button", { name: "No" })).toBeVisible()
-      await expect(page.getByRole("link", { name: "Yes" })).toBeVisible()
+      await expect(page.getByRole("button", { name: "Yes" })).toBeVisible()
     })
 
     test("should allow to cancel & exit", async ({ page, isMobile }) => {
@@ -317,7 +317,7 @@ test.describe("Admin - Create Project", () => {
       })
 
       await page.getByRole("button", { name: "Cancel" }).click()
-      await page.getByRole("link", { name: "Yes" }).click()
+      await page.getByRole("button", { name: "Yes" }).click()
 
       await page.waitForLoadState("networkidle")
 
