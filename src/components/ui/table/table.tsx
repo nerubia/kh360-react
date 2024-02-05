4299c965d7650e140c2d85a044bb30c6371aef99
Chadie Gil <caugis@nerubia.com>
2024-02-05 13:23:00 +0800

    kh360-react-621-create-table-component-refactor

---
+++ src/components/ui/table/table.tsx
@@ -0,0 +1,56 @@
+import { type ReactNode } from "react"
+
+interface TableProps<T extends { id: number }> {
+  data: T[]
+  columns: string[]
+  renderCell: (item: T, column: string) => ReactNode
+  onClickRow?: (id: number) => void
+}
+
+export function Table<T extends { id: number }>({
+  data,
+  columns,
+  renderCell,
+  onClickRow,
+}: TableProps<T>): JSX.Element {
+  const handleRowClick = (item: T) => {
+    if (onClickRow != null) {
+      onClickRow(item.id)
+    }
+  }
+  return (
+    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+      <table className='w-full text-left rtl:text-right'>
+        <thead className='text-black'>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={index} scope='col' className='pr-3 py-2 whitespace-nowrap'>
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr
+              key={index}
+              className='bg-white border-b hover:bg-slate-100 text-black hover:cursor-pointer'
+              onClick={() => handleRowClick(item)}
+            >
+              {columns.map((column) => (
+                <td
+                  key={column}
+                  className={`pr-3 py-2 ${
+                    index === columns.length - 1 ? "mid-w-100 md:w-90" : "w-1/5"
+                  }`}
+                >
+                  {renderCell(item, column)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
