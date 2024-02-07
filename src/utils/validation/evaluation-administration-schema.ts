9066668bd3465cfe4f558ad415ae4a9a0780afac
emmaacha <emmalynacha@gmail.com>
2024-02-07 11:07:16 +0800

    update schema to validate min and max allowed dates

--- src/utils/validation/evaluation-administration-schema.ts
+++ src/utils/validation/evaluation-administration-schema.ts
@@ -7,20 +7,102 @@ export const createEvaluationAdministrationSchema = object().shape({
     .test("start-date", "Start period must be before end period", function (start_date) {
       const end_date = this.parent.eval_period_end_date
       return new Date(start_date) <= new Date(end_date)
-    }),
+    })
+    .test(
+      "start-date",
+      "Must select a date that is within the valid range.",
+      function (start_date) {
+        const currentDate = new Date()
+
+        const maxDate = new Date(
+          currentDate.getFullYear() + 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        const minDate = new Date(
+          currentDate.getFullYear() - 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        return (
+          new Date(start_date) <= new Date(maxDate) && new Date(minDate) <= new Date(start_date)
+        )
+      }
+    ),
   eval_period_end_date: string()
     .required("End period is required")
     .test("end-date", "End period must not be later than start schedule", function (end_date) {
       const start_schedule = this.parent.eval_schedule_start_date
       return new Date(end_date) <= new Date(start_schedule)
+    })
+    .test("end-date", "Must select a date that is within the valid range.", function (end_date) {
+      const currentDate = new Date()
+
+      const maxDate = new Date(
+        currentDate.getFullYear() + 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      const minDate = new Date(
+        currentDate.getFullYear() - 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      return new Date(end_date) <= new Date(maxDate) && new Date(minDate) <= new Date(end_date)
     }),
   eval_schedule_start_date: string()
     .required("Start schedule is required")
     .test("start-date", "Start schedule must be before end schedule", function (start_date) {
       const end_date = this.parent.eval_schedule_end_date
+
       return new Date(start_date) <= new Date(end_date)
+    })
+    .test(
+      "start-date",
+      "Must select a date that is within the valid range.",
+      function (start_date) {
+        const currentDate = new Date()
+
+        const maxDate = new Date(
+          currentDate.getFullYear() + 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        const minDate = new Date(
+          currentDate.getFullYear() - 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        return (
+          new Date(start_date) <= new Date(maxDate) && new Date(minDate) <= new Date(start_date)
+        )
+      }
+    ),
+  eval_schedule_end_date: string()
+    .required("End schedule is required")
+    .test("end-date", "Must select a date that is within the valid range.", function (end_date) {
+      const currentDate = new Date()
+
+      const maxDate = new Date(
+        currentDate.getFullYear() + 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      const minDate = new Date(
+        currentDate.getFullYear() - 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      return new Date(end_date) <= new Date(maxDate) && new Date(minDate) <= new Date(end_date)
     }),
-  eval_schedule_end_date: string().required("End schedule is required"),
   remarks: string().required("Description is required"),
   email_subject: string().required("Email subject is required"),
   email_content: string().required("Email content is required"),
--- src/utils/validation/project-schema.ts
+++ src/utils/validation/project-schema.ts
@@ -8,12 +8,52 @@ export const createProjectSchema = object().shape({
     .test("start-date", "Start date must not be later than end date.", function (start_date) {
       const end_date = this.parent.end_date
       return new Date(start_date) <= new Date(end_date)
-    }),
+    })
+    .test(
+      "start-date",
+      "Must select a date that is within the valid range.",
+      function (start_date) {
+        const currentDate = new Date()
+
+        const maxDate = new Date(
+          currentDate.getFullYear() + 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        const minDate = new Date(
+          currentDate.getFullYear() - 50,
+          currentDate.getMonth(),
+          currentDate.getDate()
+        )
+
+        return (
+          new Date(start_date) <= new Date(maxDate) && new Date(minDate) <= new Date(start_date)
+        )
+      }
+    ),
   end_date: string()
     .required("End date is required")
     .test("end-date", "End period must not be earlier than start date.", function (end_date) {
       const start_date = this.parent.start_date
       return new Date(end_date) >= new Date(start_date)
+    })
+    .test("end-date", "Must select a date that is within the valid range.", function (end_date) {
+      const currentDate = new Date()
+
+      const maxDate = new Date(
+        currentDate.getFullYear() + 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      const minDate = new Date(
+        currentDate.getFullYear() - 50,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      )
+
+      return new Date(end_date) <= new Date(maxDate) && new Date(minDate) <= new Date(end_date)
     }),
   description: string().required("Description is required."),
   status: string().required("Status is required."),
