500e960e450059330d70da6b7f365a7371946b5f
emmaacha <emmalynacha@gmail.com>
2024-02-07 15:27:09 +0800

    remove dupes

--- src/features/evaluation-results/evaluation-results-header.tsx
+++
@@ -1,9 +0,0 @@
-import { PageTitle } from "../../components/shared/page-title"
-
-export const EvaluationResultsListHeader = () => {
-  return (
-    <div className='flex flex-col md:flex-row justify-between gap-4'>
-      <PageTitle>Evaluation Results</PageTitle>
-    </div>
-  )
-}
