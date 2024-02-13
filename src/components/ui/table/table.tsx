a3d973bb0329bfb6254b977878b32fb2b7c99693
Chadie Gil <caugis@nerubia.com>
2024-02-13 13:07:38 +0800

    kh360-react-680-external-eval-ui-issue

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -8,6 +8,7 @@ interface TableProps<T extends { id: number }> {
   isRowClickable?: boolean
   overflowXAuto?: boolean
   overflowYHidden?: boolean
+  applyFixedColWidth?: boolean
 }
 
 export function Table<T extends { id: number }>({
@@ -18,6 +19,7 @@ export function Table<T extends { id: number }>({
   isRowClickable = false,
   overflowXAuto = true,
   overflowYHidden = true,
+  applyFixedColWidth = false,
 }: TableProps<T>): JSX.Element {
   const handleRowClick = (item: T) => {
     if (onClickRow != null) {
@@ -27,7 +29,24 @@ export function Table<T extends { id: number }>({
 
   const smallColumns = ["Score", "Z-Score", "Banding", "Actions", "Default"]
   const wrapColumns = ["With Recommendation", "Evaluator Role", "Evaluee Role"]
+  const fixColWidth = ["Name", "Email Address"]
   const columnWidth = 100 / columns.length
+
+  const getColumnClassName = (column: string | ReactNode) => {
+    const isSmallColumn = smallColumns.includes(column as string)
+    const isWrapColumn = wrapColumns.includes(column as string)
+    const isFixColWidth = applyFixedColWidth && fixColWidth.includes(column as string)
+
+    if (isSmallColumn) {
+      return "whitespace-nowrap w-1/20"
+    } else if (isWrapColumn) {
+      return "whitespace-normal text-center"
+    } else if (isFixColWidth) {
+      return "whitespace-nowrap w-1/4"
+    } else {
+      return `whitespace-nowrap w-${columnWidth}`
+    }
+  }
 
   const containerClassName = `relative h-full sm:rounded-lg ${
     overflowXAuto ? "overflow-x-auto" : ""
@@ -39,17 +58,7 @@ export function Table<T extends { id: number }>({
         <thead className='text-black'>
           <tr>
             {columns.map((column, index) => (
-              <th
-                key={index}
-                scope='col'
-                className={`pr-3 py-2 ${
-                  smallColumns.includes(column as string)
-                    ? "whitespace-nowrap w-1/20"
-                    : wrapColumns.includes(column as string)
-                    ? "whitespace-normal text-center"
-                    : `whitespace-nowrap w-${columnWidth}`
-                }`}
-              >
+              <th key={index} scope='col' className={`pr-3 py-2 ${getColumnClassName(column)}`}>
                 {column}
               </th>
             ))}
