f579ae25fbef874a01cdea40f3f1d6dad726e07e
emmaacha <emmalynacha@gmail.com>
2024-02-14 16:29:38 +0800

    fix updated data lost after adding skill

--- src/features/admin/projects/create/create-project-form.tsx
+++ src/features/admin/projects/create/create-project-form.tsx
@@ -62,17 +62,19 @@ export const CreateProjectForm = () => {
 
   useEffect(() => {
     if (id !== undefined && project !== null) {
-      void appDispatch(
-        setProjectFormData({
-          name: project.name,
-          client_id: project.client?.id.toString(),
-          start_date: project.start_date?.split("T")[0],
-          end_date: project.end_date?.split("T")[0],
-          description: project.description,
-          status: project.status,
-          skill_ids: [],
-        })
-      )
+      if (projectFormData === null) {
+        void appDispatch(
+          setProjectFormData({
+            name: project.name,
+            client_id: project.client?.id.toString(),
+            start_date: project.start_date?.split("T")[0],
+            end_date: project.end_date?.split("T")[0],
+            description: project.description ?? "",
+            status: project.status,
+            skill_ids: [],
+          })
+        )
+      }
       if (selectedSkills.length === 0) {
         const skillsCopy = [...(project.project_skills ?? [])]
         const sortedSkills = skillsCopy?.sort((a, b) => {
@@ -141,17 +143,7 @@ export const CreateProjectForm = () => {
             variant: "success",
           })
         )
-        void appDispatch(
-          setProjectFormData({
-            name: "",
-            client_id: undefined,
-            start_date: "",
-            end_date: "",
-            description: "",
-            status: "",
-            skill_ids: [],
-          })
-        )
+        void appDispatch(setProjectFormData(null))
         void appDispatch(setSelectedSkills([]))
         void appDispatch(setCheckedSkills([]))
       }
@@ -196,17 +188,7 @@ export const CreateProjectForm = () => {
               variant: "success",
             })
           )
-          void appDispatch(
-            setProjectFormData({
-              name: "",
-              client_id: undefined,
-              start_date: "",
-              end_date: "",
-              description: "",
-              status: "",
-              skill_ids: [],
-            })
-          )
+          void appDispatch(setProjectFormData(null))
           void appDispatch(setSelectedSkills([]))
           void appDispatch(setCheckedSkills([]))
         }
@@ -223,17 +205,7 @@ export const CreateProjectForm = () => {
   }
 
   const handleCancel = () => {
-    void appDispatch(
-      setProjectFormData({
-        name: "",
-        client_id: undefined,
-        start_date: "",
-        end_date: "",
-        description: "",
-        status: "",
-        skill_ids: [],
-      })
-    )
+    void appDispatch(setProjectFormData(null))
     void appDispatch(setSelectedSkills([]))
     void appDispatch(setCheckedSkills([]))
     navigate(callback ?? "/admin/projects")
@@ -241,116 +213,122 @@ export const CreateProjectForm = () => {
 
   return (
     <>
-      <div className='flex flex-col gap-10 md:w-1/2'>
-        <div className='flex flex-col gap-4'>
-          <div>
-            <h2 className='font-medium'>Name</h2>
-            <Input
-              name='name'
-              placeholder='Name'
-              value={projectFormData.name}
-              onChange={handleInputChange}
-              error={validationErrors.name}
-            />
-          </div>
-          <div className='flex flex-wrap gap-4'>
-            <div className='flex-1'>
+      {((projectFormData !== null && project !== null) || project === null) && (
+        <>
+          <div className='flex flex-col gap-10 md:w-1/2'>
+            <div className='flex flex-col gap-4'>
+              <div>
+                <h2 className='font-medium'>Name</h2>
+                <Input
+                  name='name'
+                  placeholder='Name'
+                  value={projectFormData?.name}
+                  onChange={handleInputChange}
+                  error={validationErrors.name}
+                />
+              </div>
+              <div className='flex flex-wrap gap-4'>
+                <div className='flex-1'>
+                  <CustomSelect
+                    data-test-id='SelectClient'
+                    label='Client'
+                    name='client_id'
+                    value={clientOptions.find(
+                      (option) => option.value === projectFormData?.client_id
+                    )}
+                    onChange={async (option) => await onChangeClient(option)}
+                    options={clientOptions}
+                    fullWidth
+                    error={validationErrors.client_id}
+                    isClearable={true}
+                  />
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <h2 className='font-medium'>Project Duration</h2>
+                <div className='flex flex-col sm:flex-row items-center gap-4'>
+                  <div className='w-full'>
+                    <Input
+                      name='start_date'
+                      type='date'
+                      placeholder='Start date'
+                      value={projectFormData?.start_date}
+                      onChange={handleInputChange}
+                      error={validationErrors.start_date}
+                      max={projectFormData?.end_date}
+                    />
+                  </div>
+                  <h2 className='font-medium'>to</h2>
+                  <div className='w-full'>
+                    <Input
+                      name='end_date'
+                      type='date'
+                      placeholder='End date'
+                      value={projectFormData?.end_date}
+                      onChange={handleInputChange}
+                      error={validationErrors.end_date}
+                      min={projectFormData?.start_date}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col gap-4'>
+                <TextArea
+                  label='Description'
+                  name='description'
+                  placeholder='Description'
+                  value={projectFormData?.description}
+                  onChange={handleTextAreaChange}
+                  error={validationErrors.description}
+                />
+              </div>
               <CustomSelect
-                data-test-id='SelectClient'
-                label='Client'
-                name='client_id'
-                value={clientOptions.find((option) => option.value === projectFormData.client_id)}
-                onChange={async (option) => await onChangeClient(option)}
-                options={clientOptions}
-                fullWidth
-                error={validationErrors.client_id}
-                isClearable={true}
+                data-test-id='SelectProjectStatus'
+                label='Status'
+                name='project_status'
+                value={statusOptions.find((option) => option.value === projectFormData?.status)}
+                onChange={async (option) => await onChangeStatus(option)}
+                options={statusOptions}
+                fullWidth={false}
+                error={validationErrors.status}
               />
             </div>
           </div>
-          <div className='flex flex-col'>
-            <h2 className='font-medium'>Project Duration</h2>
-            <div className='flex flex-col sm:flex-row items-center gap-4'>
-              <div className='w-full'>
-                <Input
-                  name='start_date'
-                  type='date'
-                  placeholder='Start date'
-                  value={projectFormData.start_date}
-                  onChange={handleInputChange}
-                  error={validationErrors.start_date}
-                  max={projectFormData.end_date}
-                />
-              </div>
-              <h2 className='font-medium'>to</h2>
-              <div className='w-full'>
-                <Input
-                  name='end_date'
-                  type='date'
-                  placeholder='End date'
-                  value={projectFormData.end_date}
-                  onChange={handleInputChange}
-                  error={validationErrors.end_date}
-                  min={projectFormData.start_date}
-                />
-              </div>
-            </div>
+          <CreateProjectTable />
+          <div className='flex justify-between xl:w-2/3 pr-5'>
+            <Button variant='primaryOutline' onClick={toggleCancelDialog}>
+              Cancel
+            </Button>
+            <Button onClick={toggleSaveDialog}>Save</Button>
           </div>
-          <div className='flex flex-col gap-4'>
-            <TextArea
-              label='Description'
-              name='description'
-              placeholder='Description'
-              value={projectFormData.description}
-              onChange={handleTextAreaChange}
-              error={validationErrors.description}
+          <Suspense>
+            <ProjectsDialog
+              open={showSaveDialog}
+              title='Save Project'
+              description='Are you sure you want to save this project?'
+              onClose={toggleSaveDialog}
+              onSubmit={async () => {
+                await toggleSaveDialog()
+                project === null ? await handleSubmit() : await handleUpdate()
+              }}
             />
-          </div>
-          <CustomSelect
-            data-test-id='SelectProjectStatus'
-            label='Status'
-            name='project_status'
-            value={statusOptions.find((option) => option.value === projectFormData.status)}
-            onChange={async (option) => await onChangeStatus(option)}
-            options={statusOptions}
-            fullWidth={false}
-            error={validationErrors.status}
-          />
-        </div>
-      </div>
-      <CreateProjectTable />
-      <div className='flex justify-between xl:w-2/3 pr-5'>
-        <Button variant='primaryOutline' onClick={toggleCancelDialog}>
-          Cancel
-        </Button>
-        <Button onClick={toggleSaveDialog}>Save</Button>
-      </div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProjectsDialog
-          open={showSaveDialog}
-          title='Save Project'
-          description='Are you sure you want to save this project?'
-          onClose={toggleSaveDialog}
-          onSubmit={async () => {
-            await toggleSaveDialog()
-            project === null ? await handleSubmit() : await handleUpdate()
-          }}
-        />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProjectsDialog
-          open={showCancelDialog}
-          title='Cancel'
-          description={
-            <>
-              Are you sure you want to cancel? <br />
-              If you cancel, your data won&apos;t be saved.
-            </>
-          }
-          onClose={toggleCancelDialog}
-          onSubmit={handleCancel}
-        />
-      </Suspense>
+          </Suspense>
+          <Suspense>
+            <ProjectsDialog
+              open={showCancelDialog}
+              title='Cancel'
+              description={
+                <>
+                  Are you sure you want to cancel? <br />
+                  If you cancel, your data won&apos;t be saved.
+                </>
+              }
+              onClose={toggleCancelDialog}
+              onSubmit={handleCancel}
+            />
+          </Suspense>
+        </>
+      )}
     </>
   )
 }
