58baa872f1c601748759473f4d20eb478767217a
emmaacha <emmalynacha@gmail.com>
2024-02-16 11:51:32 +0800

    update view eval admin for enhancements

--- src/features/admin/evaluation-administrations/[id]/view-evaluation-list.tsx
+++ src/features/admin/evaluation-administrations/[id]/view-evaluation-list.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef, lazy, Suspense } from "react"
 import { Icon } from "@components/ui/icon/icon"
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams, useNavigate, useLocation } from "react-router-dom"
 import { useAppSelector } from "@hooks/useAppSelector"
 import { useAppDispatch } from "@hooks/useAppDispatch"
 import { formatDate, shortenFormatDate } from "@utils/format-date"
@@ -9,13 +9,22 @@ import { EvaluationAdministrationStatus } from "@custom-types/evaluation-adminis
 import { getEvaluationTemplates } from "@redux/slices/evaluation-templates-slice"
 import { getEvaluations } from "@redux/slices/evaluations-slice"
 import { type EvaluationResult } from "@custom-types/evaluation-result-type"
-import { getEvaluationResults } from "@redux/slices/evaluation-results-slice"
+import {
+  getEvaluationResults,
+  deleteEvaluationResult,
+} from "@redux/slices/evaluation-results-slice"
 import { useMobileView } from "@hooks/use-mobile-view"
+import { setAlert } from "@redux/slices/app-slice"
+import { setSelectedEmployeeIds } from "@redux/slices/evaluation-administration-slice"
+
 export const ViewEvaluationList = () => {
+  const location = useLocation()
   const appDispatch = useAppDispatch()
   const navigate = useNavigate()
   const { id } = useParams()
-  const { evaluation_administration } = useAppSelector((state) => state.evaluationAdministration)
+  const { evaluation_administration, selectedEmployeeIds } = useAppSelector(
+    (state) => state.evaluationAdministration
+  )
   const { evaluation_templates } = useAppSelector((state) => state.evaluationTemplates)
   const { evaluations } = useAppSelector((state) => state.evaluations)
   const { evaluation_results, hasNextPage } = useAppSelector((state) => state.evaluationResults)
@@ -25,7 +34,6 @@ export const ViewEvaluationList = () => {
   const [selectedEvaluationTemplateId, setSelectedEvaluationTemplateId] = useState<string>()
   const [dispatchedEmployees, setDispatchedEmployees] = useState<number[]>([])
   const [dispatchedEvaluationDetails, setDispatchedEvaluationDetails] = useState<string[]>([])
-  const [editEvaluationResult, setEditEvaluationResult] = useState<boolean>(false)
 
   const listInnerRef = useRef<HTMLDivElement>(null)
   const [currPage, setCurrPage] = useState(1)
@@ -33,6 +41,24 @@ export const ViewEvaluationList = () => {
   const [lastList, setLastList] = useState(false)
   const [isInsertingData, setIsInsertingData] = useState<boolean>(false)
   const isMobile = useMobileView()
+
+  const [showDeleteDialog, setShowDeleteDialog] = useState<boolean>(false)
+
+  const [evaluationResultToggledState, setEvaluationResultToggledState] = useState<boolean[]>([])
+
+  const [evaluationDetailsToggledState, setEvaluationDetailsToggledState] = useState<boolean[][]>(
+    Array.from(
+      {
+        length: 0,
+      },
+      () => [false]
+    )
+  )
+
+  const EvaluationAdminDialog = lazy(
+    async () =>
+      await import("@features/admin/evaluation-administrations/evaluation-administrations-dialog")
+  )
 
   useEffect(() => {
     if (id !== undefined) {
@@ -86,6 +112,43 @@ export const ViewEvaluationList = () => {
     ])
   }, [isInsertingData, evaluation_results])
 
+  useEffect(() => {
+    if (selectedEvaluationResultId !== undefined) {
+      setEvaluationResults((prevResults) =>
+        prevResults.map((result) => {
+          if (result.id === parseInt(selectedEvaluationResultId)) {
+            return { ...result, evaluation_templates }
+          }
+          return result
+        })
+      )
+    }
+  }, [evaluation_templates])
+
+  useEffect(() => {
+    if (
+      selectedEvaluationTemplateId !== undefined &&
+      selectedEvaluationResultId !== undefined &&
+      evaluations.length > 0
+    ) {
+      setEvaluationResults((prevResults) =>
+        prevResults.map((result) => {
+          if (result.id === parseInt(selectedEvaluationResultId)) {
+            const updatedTemplates = result.evaluation_templates?.map((template) => {
+              if (template.id === parseInt(selectedEvaluationTemplateId)) {
+                return { ...template, evaluation_details: evaluations }
+              }
+              return template
+            })
+
+            return { ...result, evaluation_templates: updatedTemplates }
+          }
+          return result
+        })
+      )
+    }
+  }, [evaluations])
+
   const onScroll = () => {
     if (listInnerRef.current !== null) {
       const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current
@@ -95,17 +158,6 @@ export const ViewEvaluationList = () => {
       }
     }
   }
-
-  const [evaluationResultToggledState, setEvaluationResultToggledState] = useState<boolean[]>([])
-
-  const [evaluationDetailsToggledState, setEvaluationDetailsToggledState] = useState<boolean[][]>(
-    Array.from(
-      {
-        length: 0,
-      },
-      () => [false]
-    )
-  )
 
   const toggleEvaluationResult = (index: number, evaluation_result_id: string | undefined) => {
     const updatedToggledState: boolean[] = [...evaluationResultToggledState]
@@ -123,18 +175,12 @@ export const ViewEvaluationList = () => {
     }
   }
 
-  useEffect(() => {
-    if (selectedEvaluationResultId !== undefined) {
-      setEvaluationResults((prevResults) =>
-        prevResults.map((result) => {
-          if (result.id === parseInt(selectedEvaluationResultId)) {
-            return { ...result, evaluation_templates }
-          }
-          return result
-        })
-      )
+  const toggleDeleteDialog = (evaluation_result_id: string | null) => {
+    setShowDeleteDialog((prev) => !prev)
+    if (evaluation_result_id !== null) {
+      setSelectedEvaluationResultId(evaluation_result_id)
     }
-  }, [evaluation_templates])
+  }
 
   const toggleEvaluationDetails = (
     employeeIndex: number,
@@ -167,53 +213,65 @@ export const ViewEvaluationList = () => {
     }
   }
 
-  useEffect(() => {
-    if (
-      selectedEvaluationTemplateId !== undefined &&
-      selectedEvaluationResultId !== undefined &&
-      evaluations.length > 0
-    ) {
-      setEvaluationResults((prevResults) =>
-        prevResults.map((result) => {
-          if (result.id === parseInt(selectedEvaluationResultId)) {
-            const updatedTemplates = result.evaluation_templates?.map((template) => {
-              if (template.id === parseInt(selectedEvaluationTemplateId)) {
-                return { ...template, evaluation_details: evaluations }
-              }
-              return template
-            })
-
-            return { ...result, evaluation_templates: updatedTemplates }
-          }
-          return result
+  const handleEditEvaluationResult = async (evaluation_result_id: string | undefined) => {
+    try {
+      const result = await appDispatch(
+        getEvaluationTemplates({
+          evaluation_result_id,
         })
       )
-    }
-  }, [evaluations])
-
-  const handleEditEvaluationResult = (evaluation_result_id: string | undefined) => {
-    void appDispatch(
-      getEvaluationTemplates({
-        evaluation_result_id,
-        for_evaluation: true,
-      })
-    )
-    setSelectedEvaluationResultId(evaluation_result_id)
-    setEditEvaluationResult(true)
+      if (result.type === "evaluationTemplate/getEvaluationTemplates/fulfilled") {
+        const templateId = result.payload[0].id
+        if (evaluation_administration?.status === EvaluationAdministrationStatus.Draft) {
+          navigate(
+            `/admin/evaluation-administrations/${id}/evaluees/${evaluation_result_id}/evaluators/${templateId}`
+          )
+        } else {
+          navigate(
+            `/admin/evaluation-administrations/${id}/evaluees/${evaluation_result_id}/evaluators/${templateId}?callback=${location.pathname}`
+          )
+        }
+      }
+    } catch (err) {}
   }
 
-  useEffect(() => {
-    if (editEvaluationResult && evaluation_templates[0] !== undefined) {
-      navigate(
-        `/admin/evaluation-administrations/${id}/evaluees/${selectedEvaluationResultId}/evaluators/${evaluation_templates[0]?.id}`
-      )
+  const handleDeleteEvaluee = async () => {
+    if (selectedEvaluationResultId !== undefined) {
+      try {
+        const result = await appDispatch(
+          deleteEvaluationResult(parseInt(selectedEvaluationResultId))
+        )
+        if (result.type === "evaluationResults/deleteEvaluationResult/fulfilled") {
+          appDispatch(
+            setAlert({
+              description: `Evaluee successfully removed.`,
+              variant: "success",
+            })
+          )
+          const updatedEvaluationResults = evaluationResults.filter(
+            (evaluationResult) => evaluationResult.id !== parseInt(result.payload.id)
+          )
+          setEvaluationResults(updatedEvaluationResults)
+          setEvaluationResultToggledState((prev) => prev.slice(0, -1))
+          setEvaluationDetailsToggledState((prev) => prev.slice(0, -1))
+          setSelectedEvaluationResultId(undefined)
+          appDispatch(
+            setSelectedEmployeeIds(selectedEmployeeIds.filter((id) => id !== result.payload.id))
+          )
+          toggleDeleteDialog(null)
+        }
+      } catch (error) {}
     }
-  }, [editEvaluationResult, evaluation_templates])
+  }
+
+  const handleAddEvaluee = () => {
+    navigate(`/admin/evaluation-administrations/${id}/select`)
+  }
 
   return (
     <>
       <div
-        className='flex-1 flex flex-col gap-8 overflow-y-auto md:overflow-x-hidden h-screen'
+        className='flex flex-col gap-8 overflow-y-auto md:overflow-x-hidden'
         onScroll={onScroll}
         ref={listInnerRef}
       >
@@ -222,13 +280,25 @@ export const ViewEvaluationList = () => {
             <div key={evaluationIndex} className='mb-2'>
               <div className='flex gap-2 mb-2'>
                 {(evaluation_administration?.status === EvaluationAdministrationStatus.Pending ||
-                  evaluation_administration?.status === EvaluationAdministrationStatus.Ongoing) && (
+                  evaluation_administration?.status === EvaluationAdministrationStatus.Ongoing ||
+                  evaluation_administration?.status === EvaluationAdministrationStatus.Draft) && (
                   <Button
                     testId='EditButton'
-                    onClick={() => handleEditEvaluationResult(evaluationResult.id?.toString())}
+                    onClick={async () =>
+                      await handleEditEvaluationResult(evaluationResult.id?.toString())
+                    }
                     variant={"unstyled"}
                   >
                     <Icon icon='PenSquare' />
+                  </Button>
+                )}
+                {evaluation_administration?.status === EvaluationAdministrationStatus.Draft && (
+                  <Button
+                    testId='EditButton'
+                    onClick={() => toggleDeleteDialog(evaluationResult.id?.toString())}
+                    variant={"unstyled"}
+                  >
+                    <Icon icon='Trash' />
                   </Button>
                 )}
                 <Button
@@ -257,7 +327,15 @@ export const ViewEvaluationList = () => {
                   evaluationResult.evaluation_templates !== null &&
                   evaluationResult.evaluation_templates.length > 0
                     ? evaluationResult.evaluation_templates.map((template, templateIndex) => (
-                        <div key={templateIndex} className='mb-2 ml-4'>
+                        <div
+                          key={templateIndex}
+                          className={`mb-2 ${
+                            evaluation_administration?.status ===
+                            EvaluationAdministrationStatus.Draft
+                              ? "ml-14"
+                              : "ml-4"
+                          }`}
+                        >
                           <button
                             onClick={() =>
                               toggleEvaluationDetails(
@@ -337,6 +415,36 @@ export const ViewEvaluationList = () => {
           ))}
         </div>
       </div>
+      {evaluationResults.length === 0 ? (
+        <div className='pb-4 pl-2'>
+          No evaluees added yet. Click{" "}
+          <span onClick={handleAddEvaluee} className='text-primary-500 cursor-pointer underline'>
+            {" "}
+            here
+          </span>{" "}
+          to add
+        </div>
+      ) : (
+        <div className='flex justify-start pb-10'>
+          <Button onClick={handleAddEvaluee} variant={"ghost"}>
+            <Icon icon='Plus' size='small' color='primary' />
+            <p className='text-primary-500 uppercase whitespace-nowrap text-sm'>Add Evaluee</p>
+          </Button>
+        </div>
+      )}
+      <Suspense>
+        <EvaluationAdminDialog
+          open={showDeleteDialog}
+          title='Delete Evaluee'
+          description={
+            <>
+              Are you sure you want to delete this record? <br /> This action cannot be reverted.
+            </>
+          }
+          onClose={() => toggleDeleteDialog(null)}
+          onSubmit={handleDeleteEvaluee}
+        />
+      </Suspense>
     </>
   )
 }
