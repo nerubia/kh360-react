197d0f20979cbef01a5e568eeb018da6f71cff5d
Chadie Gil <caugis@nerubia.com>
2024-02-20 09:39:00 +0800

    kh360-721-update-date-range-picker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,tsx}"],
+  content: ["./src/**/*.{html,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -112,4 +114,5 @@ module.exports = {
     },
   },
   plugins: [],
+  darkMode: ""
 }
