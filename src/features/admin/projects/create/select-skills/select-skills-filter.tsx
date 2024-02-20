f8ec6f1b0bd94d90370e30ead2ad9f556ff7eb09
emmaacha <emmalynacha@gmail.com>
2024-02-20 12:20:33 +0800

    update add skills for projects for enhancements

--- src/features/admin/projects/create/select-skills/select-skills-filter.tsx
+++ src/features/admin/projects/create/select-skills/select-skills-filter.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react"
 import { useSearchParams } from "react-router-dom"
 import { Input } from "@components/ui/input/input"
 import { Button } from "@components/ui/button/button"
-import { CustomSelect } from "@components/ui/select/custom-select"
 import { useAppSelector } from "@hooks/useAppSelector"
 import { getSkillCategories } from "@redux/slices/skill-category-slice"
 import { useAppDispatch } from "@hooks/useAppDispatch"
@@ -34,13 +33,21 @@ export const SelectSkillsFilter = () => {
     setCategoryOptions(options)
   }, [skill_categories])
 
-  const handleSearch = async () => {
-    if (name.length !== 0 || category.length !== 0) {
-      searchParams.set("name", name)
+  useEffect(() => {
+    if (category.length !== 0) {
       searchParams.set("skill_category_id", category)
     } else {
-      searchParams.delete("name")
       searchParams.delete("skill_category_id", category)
+    }
+    searchParams.set("page", "1")
+    setSearchParams(searchParams)
+  }, [category])
+
+  const handleSearch = async () => {
+    if (name.length !== 0) {
+      searchParams.set("name", name)
+    } else {
+      searchParams.delete("name")
     }
     searchParams.set("page", "1")
     setSearchParams(searchParams)
@@ -53,34 +60,40 @@ export const SelectSkillsFilter = () => {
   }
 
   return (
-    <div className='flex flex-col md:flex-row justify-between gap-4'>
-      <div className='flex-1 flex flex-col md:flex-row gap-4'>
-        <div className='w-9/20'>
-          <CustomSelect
-            data-test-id='searchCategory'
-            label='Category'
-            name='searchCategory'
-            value={categoryOptions.find((option) => option.value === category)}
-            onChange={(option) => setCategory(option !== null ? option.value : "all")}
-            options={categoryOptions}
-            fullWidth
-          />
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-col md:flex-row justify-between gap-4'>
+        <div className='flex-1 flex flex-col md:flex-row gap-4'>
+          <div className='flex-1'>
+            <Input
+              label='Name'
+              name='searchName'
+              placeholder='Search by name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
         </div>
-        <div className='flex-1'>
-          <Input
-            label='Name'
-            name='searchName'
-            placeholder='Search by name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <div className='flex items-end gap-4'>
+          <Button onClick={handleSearch}>Search</Button>
+          <Button variant='primaryOutline' onClick={handleClear}>
+            Clear
+          </Button>
         </div>
       </div>
-      <div className='flex items-end gap-4'>
-        <Button onClick={handleSearch}>Search</Button>
-        <Button variant='primaryOutline' onClick={handleClear}>
-          Clear
-        </Button>
+      <div className='flex flex-row gap-4'>
+        {categoryOptions.map((categoryOption, index) => (
+          <div key={index}>
+            <Button
+              fullWidth
+              variant={categoryOption.value === category ? "primary" : "primaryOutline"}
+              onClick={() => setCategory(categoryOption.value ?? "all")}
+              size='small'
+              fullHeight
+            >
+              {categoryOption.label}
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   )
