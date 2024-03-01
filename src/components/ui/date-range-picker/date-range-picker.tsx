dbaffb3c928c85bedbe0b36bba18e32c221949e6
Chadie Gil <caugis@nerubia.com>
2024-03-01 09:13:20 +0800

    react-723-project-assignment-implement-date-range-picker

--- src/components/ui/date-range-picker/date-range-picker.tsx
+++ src/components/ui/date-range-picker/date-range-picker.tsx
@@ -1,10 +1,17 @@
 import Datepicker, { type DateValueType } from "react-tailwindcss-datepicker"
 
+interface DateType {
+  start_date?: string | null
+  end_date?: string | null
+}
+
 interface DateRangePickerProps {
   name: string
   label?: string
   value: DateValueType
   onChange: (value: DateValueType, e?: HTMLInputElement | null | undefined) => void
+  error?: DateType | null
+  dateLimit?: DateType | null
 }
 
 export const DateRangePicker: React.FC<DateRangePickerProps> = ({
@@ -12,17 +19,36 @@ export const DateRangePicker: React.FC<DateRangePickerProps> = ({
   label,
   value,
   onChange,
+  error,
+  dateLimit,
 }: DateRangePickerProps) => {
   const currentDate = new Date()
-  const minDate = new Date(currentDate)
-  minDate.setFullYear(currentDate.getFullYear() - 50)
-  const maxDate = new Date(currentDate)
-  maxDate.setFullYear(currentDate.getFullYear() + 50)
+
+  const parseDate = (dateString: string | null): Date => {
+    return dateString != null ? new Date(dateString) : new Date()
+  }
+
+  const minDate =
+    dateLimit?.start_date != null
+      ? parseDate(dateLimit.start_date)
+      : new Date(currentDate.getFullYear() - 50, 0, 1)
+  const maxDate =
+    dateLimit?.end_date != null
+      ? parseDate(dateLimit.end_date)
+      : new Date(currentDate.getFullYear() + 50, 11, 31)
 
   const containerClassName = (defaultClassName: string) => {
-    const customStyle = "border rounded-md"
-    return `${defaultClassName} ${customStyle}`
+    const customStyle =
+      error?.start_date !== undefined || error?.end_date !== undefined ? "border-red-500" : ""
+    return `${defaultClassName} border rounded-md ${customStyle}`
   }
+
+  const isErrorPresent =
+    error != null &&
+    (error.start_date !== null || error.end_date !== null) &&
+    (error.start_date !== undefined || error.end_date !== undefined)
+
+  const sameDateErrorMessage = error?.start_date === error?.end_date
 
   return (
     <div>
@@ -46,6 +72,15 @@ export const DateRangePicker: React.FC<DateRangePickerProps> = ({
         maxDate={maxDate}
         containerClassName={containerClassName}
       />
+      {isErrorPresent && (
+        <p className='text-red-500 text-sm'>
+          {sameDateErrorMessage
+            ? `${error?.start_date != null ? error.start_date : ""}`.trim()
+            : `${error?.start_date != null ? error.start_date : ""}${
+                error?.start_date != null && error?.end_date != null ? ", " : ""
+              }${error?.end_date != null ? error.end_date : ""}`.trim()}
+        </p>
+      )}
     </div>
   )
 }
