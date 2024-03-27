b61d3064869cd3a737650f4006d6fcb29a1e8218
emmaacha <emmalynacha@gmail.com>
2024-03-27 13:14:16 +0800

    update view survey form layout

--- src/components/ui/tooltip/tooltip.tsx
+++ src/components/ui/tooltip/tooltip.tsx
@@ -3,7 +3,7 @@ import { type VariantProps, cva } from "class-variance-authority"
 import { TooltipTrigger } from "@components/ui/tooltip/tooltip-trigger"
 import { TooltipContent } from "@components/ui/tooltip/tooltip-content"
 
-const tooltip = cva(["absolute", "z-50", "w-max", "max-w-md", "invisible", "group-hover:visible"], {
+const tooltip = cva(["absolute", "z-50", "invisible", "group-hover:visible"], {
   variants: {
     placement: {
       top: ["bottom-full", "left-1/2", "-translate-x-1/2", "pb-1"],
@@ -15,19 +15,25 @@ const tooltip = cva(["absolute", "z-50", "w-max", "max-w-md", "invisible", "grou
       left: ["top-1/2", "-translate-y-1/2", "right-full", "pr-1"],
       right: ["top-1/2", "-translate-y-1/2", "left-full", "pl-1"],
     },
+    size: {
+      default: ["w-max", "max-w-md"],
+      medium: ["break-word", "min-w-[140px]"],
+    },
   },
   defaultVariants: {
     placement: "top",
+    size: "default",
   },
 })
 
 interface TooltipProps extends VariantProps<typeof tooltip> {
   children: React.ReactNode
+  wFit?: boolean
 }
 
-function Tooltip({ children, placement }: TooltipProps) {
+function Tooltip({ children, placement, size, wFit = true }: TooltipProps) {
   return (
-    <div className='relative w-fit'>
+    <div className={`relative ${wFit ? "w-fit" : ""}`}>
       <div className='group'>
         {React.Children.map(children, (child) => {
           if (React.isValidElement(child) && child.type === Tooltip.Trigger) {
@@ -35,7 +41,7 @@ function Tooltip({ children, placement }: TooltipProps) {
           }
           return null
         })}
-        <div className={tooltip({ placement })}>
+        <div className={tooltip({ placement, size })}>
           <div className='bg-customYellow-300 text-customBrown-500 text-xs border border-customYellow-400 rounded-md p-1.5'>
             {React.Children.map(children, (child) => {
               if (React.isValidElement(child) && child.type === Tooltip.Content) {
