25904e5bc756fff6af0b4ffd183b25ee78c335b7
emmaacha <emmalynacha@gmail.com>
2024-03-27 16:52:17 +0800

    update view survey form screen for enhancements

--- src/custom-types/survey-answer-type.ts
+++ src/custom-types/survey-answer-type.ts
@@ -5,6 +5,7 @@ export interface SurveyAnswer {
   survey_template_answer_id?: number
   survey_template_question_id?: number
   answer_text?: string
+  amount?: string | number
   survey_template_answers?: SurveyTemplateAnswer
 }
 
