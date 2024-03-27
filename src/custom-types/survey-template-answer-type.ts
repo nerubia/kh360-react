dbcd87975ab764ab7428b64b8088dd2c455bea6a
emmaacha <emmalynacha@gmail.com>
2024-03-27 10:13:40 +0800

    update survey form images link

--- src/custom-types/survey-template-answer-type.ts
+++ src/custom-types/survey-template-answer-type.ts
@@ -1,5 +1,6 @@
 export interface SurveyTemplateAnswer {
   id?: number
+  survey_template_id?: number | string
   sequence_no?: number
   answer_text?: string
   answer_description?: string
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,3 @@
-export const getSurveyImage = (file_name: string, survey_admin_id: string) => {
-  return `https://kh360-assets-beta.s3.ap-southeast-1.amazonaws.com/surveys/${survey_admin_id}/${file_name}`
+export const getSurveyImage = (file_name: string, survey_template_id: string) => {
+  return `https://kh360-assets-beta.s3.ap-southeast-1.amazonaws.com/surveys/${survey_template_id}/${file_name}`
 }
