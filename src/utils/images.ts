6902891c79184b3311843e4f9a34020b41749743
emmaacha <emmalynacha@gmail.com>
2024-04-01 18:24:05 +0800

    update link for kh360 images for survey template answers

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,3 @@
 export const getSurveyImage = (file_name: string, survey_template_id: string) => {
-  return `https://kh360-assets-beta.s3.ap-southeast-1.amazonaws.com/surveys/${survey_template_id}/${file_name}`
+  return `https://kh360-assets.s3.ap-southeast-1.amazonaws.com/surveys/${survey_template_id}/${file_name}`
 }
