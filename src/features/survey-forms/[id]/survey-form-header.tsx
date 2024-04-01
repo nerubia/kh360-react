257ee494515cd98f96b4739e0b18d17d7a0737b4
emmaacha <emmalynacha@gmail.com>
2024-04-01 14:59:41 +0800

    update answer survey form for enhancements

--- src/features/survey-forms/[id]/survey-form-header.tsx
+++ src/features/survey-forms/[id]/survey-form-header.tsx
@@ -1,12 +1,24 @@
 import { PageTitle } from "@components/shared/page-title"
 import { useAppSelector } from "@hooks/useAppSelector"
+import { Badge } from "@components/ui/badge/badge"
+import { getEvaluationResultStatusVariant } from "@utils/variant"
 
 export const SurveyFormHeader = () => {
-  const { user_survey_administrations } = useAppSelector((state) => state.user)
+  const { user_survey_administrations, survey_result_status } = useAppSelector(
+    (state) => state.user
+  )
 
   return (
     <div className='flex md:flex-col justify-between gap-4'>
-      <PageTitle>{user_survey_administrations[0]?.name}</PageTitle>
+      <div className='flex gap-4 items-center'>
+        <PageTitle>{user_survey_administrations[0]?.name}</PageTitle>
+        <Badge
+          size={"medium"}
+          variant={getEvaluationResultStatusVariant(survey_result_status ?? "")}
+        >
+          <div className='uppercase'>{survey_result_status}</div>
+        </Badge>
+      </div>
       <div>{user_survey_administrations[0]?.remarks}</div>
     </div>
   )
