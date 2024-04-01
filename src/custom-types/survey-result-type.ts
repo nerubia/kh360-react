58d896477450198a6c403891d798781f2503a135
Chadie Gil <caugis@nerubia.com>
2024-04-01 14:56:22 +0800

    react-906-survey-result-list-screen

--- src/custom-types/survey-result-type.ts
+++ src/custom-types/survey-result-type.ts
@@ -19,6 +19,7 @@ export interface SurveyResultFilters {
   status?: string
 }
 
+export const columns = ["Name", "Description", "Schedule"]
 export enum SurveyResultStatus {
   ForReview = "For Review",
   Draft = "Draft",
