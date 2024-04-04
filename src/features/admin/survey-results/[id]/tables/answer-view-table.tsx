767d06a35c2b5ec9ca7242e08c6fdea345a59628
Chadie Gil <caugis@nerubia.com>
2024-04-04 14:47:12 +0800

    react-962-survey-results-ui-enchancement

--- src/features/admin/survey-results/[id]/tables/answer-view-table.tsx
+++ src/features/admin/survey-results/[id]/tables/answer-view-table.tsx
@@ -2,15 +2,16 @@ import React, { useState, lazy, Suspense } from "react"
 import { useParams } from "react-router-dom"
 import { useAppSelector } from "@hooks/useAppSelector"
 import { Loading } from "@custom-types/loadingType"
-// import Tooltip from "@components/ui/tooltip/tooltip"
 import { Icon } from "@components/ui/icon/icon"
 import { type SurveyAnswer } from "@custom-types/survey-answer-type"
+import { ToggleSwitch } from "@components/ui/toggle-switch/toggle-switch"
 
 export const AnswerViewTable = () => {
   const { id } = useParams()
   const { loading, survey_results_answers } = useAppSelector((state) => state.surveyResults)
   const [showDetailsDialog, setShowDetailsDialog] = useState<boolean>(false)
   const [selectedAnswer, setSelectedAnswer] = useState<SurveyAnswer>()
+  const [showDetails, setShowDetails] = useState<boolean>(false)
 
   const ViewSurveyResultsDialog = lazy(
     async () => await import("@features/admin/survey-results/[id]/view-survey-results-dialog")
@@ -19,6 +20,20 @@ export const AnswerViewTable = () => {
   const toggleDialog = (selectedAnswer: SurveyAnswer | undefined) => {
     setSelectedAnswer(selectedAnswer)
     setShowDetailsDialog((prev) => !prev)
+  }
+
+  const toggleDetails = () => {
+    return setShowDetails((prev) => !prev)
+  }
+
+  const calculateOverallTotal = () => {
+    let overallTotal = 0
+    for (const answer of survey_results_answers) {
+      if (answer.subTotal !== undefined) {
+        overallTotal += answer.subTotal
+      }
+    }
+    return overallTotal
   }
 
   return (
@@ -33,32 +48,79 @@ export const AnswerViewTable = () => {
           survey_results_answers.length > 0 &&
           id !== undefined && (
             <>
-              <div className='overflow-x-auto'>
-                <table className='w-full whitespace-nowrap'>
-                  <thead>
+              <div className='relative overflow-x-auto'>
+                <table className='w-full text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                  <thead className='text-md text-primary-500 bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                     <tr>
-                      <th className='py-1 border-b-4 text-primary-500 px-2 md:w-1/4'>Item</th>
-                      <th className='py-1 border-b-4 text-primary-500 px-2 md:w-1/4'>Quantity</th>
-                      <th className='py-1 border-b-4 text-primary-500 px-2 md:w-/4'>Details</th>
+                      <th scope='col' className='py-3 w-1/3'>
+                        Category
+                      </th>
+                      <th scope='col' className='py-3'>
+                        Item
+                      </th>
+                      <th scope='col' className='py-3 text-center'>
+                        Quantity
+                      </th>
+                      <th scope='col' className='py-3 text-right w-1/20'>
+                        Price
+                      </th>
+                      <th scope='col' className='w-1/30'></th>
+                      <th scope='col' className='py-3 w-1/30 text-right'>
+                        Subtotal
+                      </th>
+                      <th scope='col' className='w-1/30'></th>
+                      <th scope='col' className='py-3'>
+                        <div className='flex gap-4 justify-start'>
+                          Details
+                          <ToggleSwitch checked={showDetails} onChange={toggleDetails} />
+                        </div>
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
                     {survey_results_answers.map((answer) => (
                       <React.Fragment key={answer.id}>
                         <tr className='hover:bg-slate-100'>
-                          <td className='py-1 px-2 border-b w-1/4 text-center'>
+                          <td className='py-1 border-b'>
+                            {answer.survey_template_answers?.survey_template_categories?.name}
+                          </td>
+                          <td className='py-1 border-b'>
                             {answer.survey_template_answers?.answer_text}
                           </td>
-                          <td className='py-1 px-2 border-b w-1/4 text-center'>
-                            {answer.totalCount}
+                          <td className='py-1 border-b text-center'>
+                            {answer.totalCount?.toLocaleString()}
                           </td>
-                          <td className='py-1 px-2 border-b w-1/4 text-center'>
-                            <div
-                              className='flex items-center justify-center cursor-pointer'
-                              onClick={() => toggleDialog(answer)}
-                            >
-                              <Icon icon='Info' size='extraSmall' color='primary' />
-                            </div>
+                          <td className='py-1 border-b text-right'>
+                            {answer.survey_template_answers?.amount?.toLocaleString()}
+                          </td>
+                          <td className='py-1 border-b text-right'></td>
+                          <td className='py-1 border-b text-right w-1/30'>
+                            {answer.subTotal?.toLocaleString()}
+                          </td>
+                          <td className='py-1 border-b text-right'></td>
+                          <td className='py-1 border-b'>
+                            {showDetails ? (
+                              <div className='ml-2'>
+                                {answer?.users?.map((user, index) => (
+                                  <div key={index}>
+                                    - {user.last_name}, {user.first_name}
+                                  </div>
+                                ))}
+                                {answer?.companion_users?.map((user, index) => (
+                                  <div key={index}>
+                                    - {user.last_name}, {user.first_name} (Companion of{" "}
+                                    {user.related_user?.first_name})
+                                  </div>
+                                ))}
+                              </div>
+                            ) : (
+                              <div
+                                className='flex cursor-pointer justify-start'
+                                onClick={() => toggleDialog(answer)}
+                              >
+                                <Icon icon='Info' size='extraSmall' color='primary' />
+                              </div>
+                            )}
                           </td>
                         </tr>
                       </React.Fragment>
@@ -66,6 +128,7 @@ export const AnswerViewTable = () => {
                   </tbody>
                 </table>
               </div>
+
               <Suspense>
                 <ViewSurveyResultsDialog
                   open={showDetailsDialog}
@@ -75,6 +138,12 @@ export const AnswerViewTable = () => {
                       {selectedAnswer?.users?.map((user, index) => (
                         <div key={index}>
                           - {user.last_name}, {user.first_name}
+                        </div>
+                      ))}
+                      {selectedAnswer?.companion_users?.map((user, index) => (
+                        <div key={index}>
+                          - {user.last_name}, {user.first_name} (Companion of{" "}
+                          {user.related_user?.first_name})
                         </div>
                       ))}
                     </>
@@ -87,6 +156,11 @@ export const AnswerViewTable = () => {
             </>
           )}
       </div>
+      {calculateOverallTotal() > 0 && (
+        <div className='flex justify-start mt-2 text-gray-500'>
+          <b>{`Overall Total: ₱${calculateOverallTotal().toLocaleString()}`}</b>
+        </div>
+      )}
     </>
   )
 }
--- src/redux/slices/survey-results-slice.ts
+++ src/redux/slices/survey-results-slice.ts
@@ -92,6 +92,7 @@ interface InitialState {
   loading_send: Loading.Idle | Loading.Pending | Loading.Fulfilled | Loading.Rejected
   error: string | null
   survey_results: SurveyResult[]
+  companion_survey_results: SurveyResult[]
   survey_results_answers: SurveyAnswer[]
   hasPreviousPage: boolean
   hasNextPage: boolean
@@ -104,6 +105,7 @@ const initialState: InitialState = {
   loading_send: Loading.Idle,
   error: null,
   survey_results: [],
+  companion_survey_results: [],
   survey_results_answers: [],
   hasPreviousPage: false,
   hasNextPage: false,
@@ -146,7 +148,8 @@ const surveyResultsSlice = createSlice({
     builder.addCase(getResultsByRespondent.fulfilled, (state, action) => {
       state.loading = Loading.Fulfilled
       state.error = null
-      state.survey_results = action.payload
+      state.survey_results = action.payload.surveyResults
+      state.companion_survey_results = action.payload.companionResults
     })
     builder.addCase(getResultsByRespondent.rejected, (state, action) => {
       state.loading = Loading.Rejected
