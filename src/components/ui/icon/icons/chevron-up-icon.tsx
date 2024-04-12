c39fb5eaa85ae5eefecbedd3f13b5ed6c4cf6c7e
Chadie Gil <caugis@nerubia.com>
2024-04-12 10:27:09 +0800

    react-897-side-menu-accordion-toggle

--- src/components/ui/icon/icons/chevron-up-icon.tsx
+++ src/components/ui/icon/icons/chevron-up-icon.tsx
@@ -7,7 +7,7 @@ export default function ChevronUpIcon({ size }: { size: number }) {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      strokeWidth='2'
+      strokeWidth='1'
       strokeLinecap='round'
       strokeLinejoin='round'
     >
