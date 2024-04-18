4049b05ad688fa7cfe85f08d4bfff483fdb243e9
emmaacha <emmalynacha@gmail.com>
2024-04-18 16:20:16 +0800

    fix dialog background size bug on diff screen size

--- src/components/ui/dialog/dialog.tsx
+++ src/components/ui/dialog/dialog.tsx
@@ -3,6 +3,7 @@ import { type VariantProps, cva } from "class-variance-authority"
 import { DialogActions } from "@components/ui/dialog/dialog-actions"
 import { DialogDescription } from "@components/ui/dialog/dialog-description"
 import { DialogTitle } from "@components/ui/dialog/dialog-title"
+import { useMobileView } from "@hooks/use-mobile-view"
 
 const dialog = cva(["w-full", "flex", "flex-col", "gap-4", "p-5", "rounded-md", "max-h-full"], {
   variants: {
@@ -33,9 +34,12 @@ interface DialogProps extends VariantProps<typeof dialog> {
 
 function Dialog({ open, children, variant, size, maxWidthMin }: DialogProps) {
   const { activeSidebar } = useAppSelector((state) => state.app)
+
+  const isMediumSize = useMobileView(1028)
+
   return open ? (
     <div className='fixed top-0 left-0 z-50 w-full h-full transition-all duration-300'>
-      <div className={`${activeSidebar ? "md:ml-64" : ""} h-full`}>
+      <div className={`${activeSidebar ? (isMediumSize ? "md:ml-44" : "md:ml-64") : ""} h-full`}>
         <div className='bg-black/50 w-full h-full flex justify-center items-center p-5'>
           <div className={dialog({ variant, size, maxWidthMin })}>{children}</div>
         </div>
