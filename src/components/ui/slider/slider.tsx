897b2597283bd1b92e96192ecc83e8b34cb7d155
Chadie Gil <caugis@nerubia.com>
2024-05-06 11:20:37 +0800

    react-1065-enhance-slider-component

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,3 +1,4 @@
+import { useMobileView } from "@hooks/use-mobile-view"
 import { type VariantProps, cva } from "class-variance-authority"
 
 const slider = cva(
@@ -10,7 +11,7 @@ const slider = cva(
     "cursor-pointer",
     "slider-thumb:appearance-none",
     "moz-slider-thumb:appearance-none",
-    "z-30",
+    "z-10",
     "slider-thumb:rounded-full",
     "moz-slider-thumb:rounded-full",
   ],
@@ -57,15 +58,24 @@ export const Slider = ({
   max = 3,
 }: SliderProps) => {
   const steps = max - min
+  let backgroundWidth
+  const isMobile = useMobileView()
+
+  if (sliderValue !== undefined) {
+    const normalizedValue = Math.max(Math.min(sliderValue, max), min)
+    backgroundWidth = ((normalizedValue - min) / (max - min)) * 100
+  } else {
+    backgroundWidth = 100
+  }
 
   const rangePoints = Array.from({ length: steps + 1 }, (_, index) => {
-    const left = index === 0 ? 0 : (index / steps) * 100 - 3
+    const left = index === 0 ? 0 : (index / steps) * 100 - (isMobile ? 4 : 3)
     const pointPosition = index === steps ? { right: 0 } : { left: `${left}%` }
 
     return (
       <div
         key={index}
-        className={`absolute top-1 w-5 h-5 bg-gray-200 border-2 border-gray-200 rounded-full z-10 hover:bg-primary-300 cursor-pointer`}
+        className={`absolute top-1 w-5 h-5 bg-gray-200 border-2 border-primary-200 rounded-full z-100 hover:bg-primary-300 cursor-pointer`}
         style={pointPosition}
       ></div>
     )
@@ -73,6 +83,10 @@ export const Slider = ({
 
   return (
     <div className='relative w-full'>
+      <div
+        className={`absolute top-2.5 h-2 bg-primary-500 rounded-full z-50 pointer-events-none`}
+        style={{ width: `${backgroundWidth}%` }}
+      ></div>
       <input
         type='range'
         min={min}
