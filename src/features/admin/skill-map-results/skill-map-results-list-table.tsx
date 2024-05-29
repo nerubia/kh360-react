0fe52a95a13d8a41f8681529e15f2de0d68984bb
Chadie Gil <caugis@nerubia.com>
2024-05-29 15:41:37 +0800

    react-1115-create-integrate-api-skill-map-result-screen

--- src/features/admin/skill-map-results/skill-map-results-list-table.tsx
+++ src/features/admin/skill-map-results/skill-map-results-list-table.tsx
@@ -6,36 +6,29 @@ import { getCmEvaluationResults } from "@redux/slices/evaluation-results-slice"
 import { Pagination } from "@components/shared/pagination/pagination"
 import { Table } from "@components/ui/table/table"
 import { formatDate } from "@utils/format-date"
-import { type SkillMapResult, columns } from "@custom-types/skill-map-result-type"
+import { columns } from "@custom-types/skill-map-result-type"
+import { getUserLatestSkillMapRatings } from "@redux/slices/user-slice"
+import { type SkillMapResultLatest } from "@custom-types/skill-map-result-latest"
+import { LineGraph } from "@components/ui/linegraph/linegraph"
 
 export const SkillMapResultsListTable = () => {
   const [searchParams] = useSearchParams()
 
   const appDispatch = useAppDispatch()
   const [showSkillMapModal, setShowSkillMapModal] = useState<boolean>(false)
+  const [selectedSkillMapResult, setSelectedSkillMapResult] = useState<SkillMapResultLatest | null>(
+    null
+  )
 
   const { hasPreviousPage, hasNextPage, totalPages } = useAppSelector(
     (state) => state.skillMapResults
   )
-
+  const { user_latest_skill_map_result, user_latest_skill_map_result_filtered } = useAppSelector(
+    (state) => state.user
+  )
   const SkillMapResultsDialog = lazy(
     async () => await import("@features/admin/skill-map-results/skill-map-results-dialog")
   )
-
-  // Hard coded dummy data for skill_map_results
-  // Please delete this after API integration
-
-  const skill_map_results = [
-    {
-      id: 1,
-      users: {
-        id: 1,
-        first_name: "Sample",
-        last_name: "User",
-      },
-      last_skill_map_date: "2024-01-01",
-    },
-  ]
 
   useEffect(() => {
     void appDispatch(
@@ -50,31 +43,41 @@ export const SkillMapResultsListTable = () => {
     )
   }, [searchParams])
 
+  useEffect(() => {
+    void appDispatch(getUserLatestSkillMapRatings())
+  }, [])
+
   const toggleSkillMapModal = () => {
     setShowSkillMapModal((prev) => !prev)
   }
 
   const handleViewSkillMapResult = (id: number) => {
-    toggleSkillMapModal()
-    /* eslint-disable */
-    // Call api to get skill map result details here
-    console.log("ID: ", id)
+    const skillMapResult = user_latest_skill_map_result.find((result) => result.id === id)
+    if (skillMapResult != null) {
+      setSelectedSkillMapResult(skillMapResult)
+      toggleSkillMapModal()
+    }
   }
 
-  const renderCell = (item: SkillMapResult, column: unknown) => {
+  const renderCell = (item: SkillMapResultLatest, column: unknown) => {
     switch (column) {
       case "Employee Name":
         return `${item.users?.last_name} ${item.users?.first_name}`
       case "Latest Skill Map":
-        return `${formatDate(item.last_skill_map_date)}`
+        return `${formatDate(item.submitted_date)}`
     }
   }
+
   return (
     <>
       <div className='flex flex-col gap-8 overflow-x-auto'>
         <Table
           columns={columns}
-          data={skill_map_results}
+          data={
+            user_latest_skill_map_result_filtered.length === 0 && searchParams.size === 0
+              ? user_latest_skill_map_result
+              : user_latest_skill_map_result_filtered
+          }
           isRowClickable={true}
           renderCell={renderCell}
           onClickRow={handleViewSkillMapResult}
@@ -92,8 +95,14 @@ export const SkillMapResultsListTable = () => {
       <Suspense>
         <SkillMapResultsDialog
           open={showSkillMapModal}
-          title='Skill Map Result Details'
-          description={<></>}
+          title='Skill Map Details'
+          description={
+            selectedSkillMapResult != null ? (
+              <>
+                <LineGraph id={selectedSkillMapResult.users.id} />
+              </>
+            ) : null
+          }
           onSubmit={toggleSkillMapModal}
         />
       </Suspense>
