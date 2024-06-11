a0b05093c6bd429f77d691d56eebd3c3a0b6c807
jatnerubia <jlerit@nerubia.com>
2024-06-11 15:37:52 +0800

    update: add skill map period and schedule

--- src/features/skill-map-forms/[id]/skill-map-form-header.tsx
+++ src/features/skill-map-forms/[id]/skill-map-form-header.tsx
@@ -7,10 +7,13 @@ import { getUserSkillMapRatings } from "@redux/slices/user-slice"
 import { useParams, useNavigate } from "react-router-dom"
 import { useAppSelector } from "@hooks/useAppSelector"
 import { SkillMapAdminStatus } from "@custom-types/skill-map-admin-type"
+import { DateRangeDisplay } from "@components/shared/display-range-date"
+import { useMobileView } from "@hooks/use-mobile-view"
 
 export const SkillMapFormHeader = () => {
   const appDispatch = useAppDispatch()
   const navigate = useNavigate()
+  const isMobile = useMobileView()
   const { user_skill_map_admins, skill_map_result_status } = useAppSelector((state) => state.user)
   const { id } = useParams()
 
@@ -52,6 +55,20 @@ export const SkillMapFormHeader = () => {
               <div className='uppercase'>{skill_map_result_status}</div>
             </Badge>
           </div>
+        </div>
+        <div>
+          <DateRangeDisplay
+            label='Skill Map Period'
+            startDate={user_skill_map_admins[0]?.skill_map_period_start_date}
+            endDate={user_skill_map_admins[0]?.skill_map_period_end_date}
+            isMobile={isMobile}
+          />
+          <DateRangeDisplay
+            label='Skill Map Schedule'
+            startDate={user_skill_map_admins[0]?.skill_map_schedule_start_date}
+            endDate={user_skill_map_admins[0]?.skill_map_schedule_end_date}
+            isMobile={isMobile}
+          />
         </div>
         <div>{user_skill_map_admins[0]?.remarks}</div>
       </div>
