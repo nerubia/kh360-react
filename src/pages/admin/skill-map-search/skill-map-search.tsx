6f5eb5de6c6a711f6375d70728602fcd897a47e0
jatnerubia <jlerit@nerubia.com>
2024-06-13 10:29:47 +0800

    update: add total results in skill map search

--- src/pages/admin/skill-map-search/skill-map-search.tsx
+++ src/pages/admin/skill-map-search/skill-map-search.tsx
@@ -1,3 +1,4 @@
+import { SkillMapSearchAction } from "@features/admin/skill-map-search/skill-map-search-actions"
 import { SkillMapSearchFilter } from "@features/admin/skill-map-search/skill-map-search-filter"
 import { SkillMapSearchHeader } from "@features/admin/skill-map-search/skill-map-search-header"
 import { SkillMapSearchTable } from "@features/admin/skill-map-search/skill-map-search-table"
@@ -10,6 +11,7 @@ export default function SkillMapSearch() {
     <div className='flex flex-col gap-8'>
       <SkillMapSearchHeader />
       <SkillMapSearchFilter />
+      <SkillMapSearchAction />
       <SkillMapSearchTable />
     </div>
   )
