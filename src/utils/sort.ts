928d535da45cde6c45605413464184f5e2428c8c
jatnerubia <jlerit@nerubia.com>
2024-06-20 15:05:04 +0800

    skill dropdown sorting

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,4 +1,6 @@
 import { type AnswerOption } from "@custom-types/answer-option-type"
+import { type SkillCategory } from "@custom-types/skill-category-type"
+import { type Skill } from "@custom-types/skill-type"
 
 export const sortAnswerOptionBySequenceNumber = (
   answerOptions: AnswerOption[],
@@ -9,6 +11,29 @@ export const sortAnswerOptionBySequenceNumber = (
     const answerOptionB = b.sequence_no
     if (answerOptionA < answerOptionB) return direction === "asc" ? -1 : 1
     if (answerOptionA > answerOptionB) return direction === "asc" ? 1 : -1
+    return 0
+  })
+}
+
+export const sortSkillCategoriesBySequenceNumber = (
+  skillCategories: SkillCategory[],
+  direction: "asc" | "desc" = "asc"
+) => {
+  return skillCategories.sort((a, b) => {
+    const nameA = a.sequence_no ?? 0
+    const nameB = b.sequence_no ?? 0
+    if (nameA < nameB) return direction === "asc" ? -1 : 1
+    if (nameA > nameB) return direction === "asc" ? 1 : -1
+    return 0
+  })
+}
+
+export const sortSkillsByName = (skills: Skill[], direction: "asc" | "desc" = "asc") => {
+  return skills.sort((a, b) => {
+    const nameA = a.name.toUpperCase()
+    const nameB = b.name.toUpperCase()
+    if (nameA < nameB) return direction === "asc" ? -1 : 1
+    if (nameA > nameB) return direction === "asc" ? 1 : -1
     return 0
   })
 }
