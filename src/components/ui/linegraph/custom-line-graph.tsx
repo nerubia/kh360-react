e88269c3c435982ae2cbb507a2f61e7a3f63f1c3
jatnerubia <jlerit@nerubia.com>
2024-06-25 11:09:11 +0800

    fix: skill map graph issue

--- src/components/ui/linegraph/custom-line-graph.tsx
+++ src/components/ui/linegraph/custom-line-graph.tsx
@@ -24,35 +24,37 @@ export const CustomLineGraph = ({ scaleYLabels, data }: CustomLineGraphProps) =>
   const [options, setOptions] = useState<ChartOptions<"line">>()
 
   useEffect(() => {
-    setOptions({
-      responsive: true,
-      plugins: {
-        legend: {
-          position: "bottom" as const,
-        },
-        tooltip: {
-          callbacks: {
-            label: (context) => {
-              return `${context.dataset.label}: ${scaleYLabels[context.raw as number]}`
+    if (scaleYLabels.length > 0) {
+      setOptions({
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "bottom" as const,
+          },
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                return `${context.dataset.label}: ${scaleYLabels[context.raw as number]}`
+              },
             },
           },
         },
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-          min: 0,
-          max: scaleYLabels.length - 1,
-          labels: scaleYLabels,
-          ticks: {
-            stepSize: 1,
-            callback: function (value) {
-              return scaleYLabels[value as number]
+        scales: {
+          y: {
+            beginAtZero: true,
+            min: 0,
+            max: scaleYLabels.length - 1,
+            labels: scaleYLabels,
+            ticks: {
+              stepSize: 1,
+              callback: function (value) {
+                return scaleYLabels[value as number]
+              },
             },
           },
         },
-      },
-    })
+      })
+    }
   }, [scaleYLabels])
 
   return <Line options={options} data={data} />
