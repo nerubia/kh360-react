1f9b51ebe4c5fdd26fb45eb2bc4d28a6018b61f3
jatnerubia <jlerit@nerubia.com>
2024-07-10 09:51:26 +0800

    fix: other skill name input validation

--- src/utils/validation/other-skill-schema.ts
+++ src/utils/validation/other-skill-schema.ts
@@ -2,6 +2,7 @@ import { object, string } from "yup"
 
 export const createOtherSkillSchema = object().shape({
   other_skill_name: string()
+    .trim()
     .required("Name is required")
     .max(255, "Name should not exceed 255 characters."),
 })
