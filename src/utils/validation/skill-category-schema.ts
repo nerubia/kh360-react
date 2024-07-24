1496f65ef92503f3d89949d927a247a7db7c0a4d
Chadie Gil <caugis@nerubia.com>
2024-07-24 14:57:12 +0800

    react-1372-edit-skill-not-required

--- src/utils/validation/skill-category-schema.ts
+++ src/utils/validation/skill-category-schema.ts
@@ -3,5 +3,5 @@ import { boolean, object, string } from "yup"
 export const createSkillCategorySchema = object().shape({
   name: string().required("Name is required").max(255, "Name should not exceed 255 characters."),
   status: boolean().required(),
-  description: string(),
+  description: string().nullable(),
 })
