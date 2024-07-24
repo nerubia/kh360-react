1d88c899485de470449a5ddb566430e33c3563fd
jatnerubia <jlerit@nerubia.com>
2024-07-24 16:16:28 +0800

    update: schema endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ import { axiosInstance } from "@utils/axios-instance"
 
 export const refreshUserToken = async () => await axiosPublic.get("/auth/refresh")
 
-export const getSchema = async () => await axiosInstance.get("/")
+export const getSchema = async () => await axiosInstance.get("/admin/json-schema")
 export const getProfile = async () => await axiosInstance.get("/user/profile")
 export const sendMail = async () => await axiosInstance.get("/user/mail")
